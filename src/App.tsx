1bd7428d8881b0ff616bd9443261f240ed312d91
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2023-12-18 19:54:36 -0600

    [3] Added Dark Mode functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { Button } from '@/components/core';
-import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <Button />
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-blue-600 text-5xl font-extrabold'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <h1 className="text-3xl font-bold dark:bg-slate-800 dark:text-white">
+      Hello world!
+    </h1>
   )
 }
 
