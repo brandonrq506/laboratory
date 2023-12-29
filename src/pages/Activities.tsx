35a612b241a047d794e6e42c2f298171d55e21d9
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2023-12-28 20:55:16 -0600

    [16] Created dummy pages

---
+++ src/pages/Activities.tsx
@@ -0,0 +1,7 @@
+export const Activities = () => {
+  return (
+    <div className="h-screen rounded-lg border bg-gray-200 text-center">
+      Activities
+    </div>
+  );
+};
---
+++ src/pages/Projects.tsx
@@ -0,0 +1,7 @@
+export const Projects = () => {
+  return (
+    <div className="h-screen rounded-lg border bg-gray-200 text-center">
+      Projects
+    </div>
+  );
+};
