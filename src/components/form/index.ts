2a0fa27b2f620e672ed2964b87aa8d5de19b166b
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2024-01-15 16:31:24 -0600

    [34] Moved ComboBox to Experiment Folder.

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -2,5 +2,5 @@ export * from "./TextArea/TextArea";
 export * from "./TextInput/TextInput";
 export * from "./InputWrapper/InputWrapper";
 export * from "./FileInput/FileInput";
-export * from "./ComboBox/ComboBox";
-export * from "./ComboBox/ComboBoxV2";
+export * from "../../experiments/ComboBox/ComboBox";
+export * from "../../experiments/ComboBox/ComboBoxV2";
