2a8c3fb94c57c0719e9341cf6cb2c317320af723
Brandon Ramirez <darkrahd@gmail.com>
2024-02-08 08:49:00 -0600

    [40] Added Select Component

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -3,3 +3,4 @@ export * from "./TextInput/TextInput";
 export * from "./InputWrapper/InputWrapper";
 export * from "./FileInput/FileInput";
 export * from "./NumerInput/NumberInput";
+export * from './Select/Select';
