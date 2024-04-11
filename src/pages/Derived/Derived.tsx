3d51dc44b277d313a01dac9846ba2673fdda1f1d
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-04-11 14:12:03 -0600

    Update derived

--- src/pages/Derived/Derived.tsx
+++ src/pages/Derived/Derived.tsx
@@ -16,17 +16,26 @@ export const Derived = () => {
   }
 
   return (
-    <>
-      <h2>Let's check you in</h2>
+    <div className="mt-16 text-center">
       <label>
-        First name: <input value={firstName} onChange={handleFirstNameChange} />
+        First name:{" "}
+        <input
+          value={firstName}
+          onChange={handleFirstNameChange}
+          className="my-1 rounded-md border border-gray-300 p-1"
+        />
       </label>
       <label>
-        Last name: <input value={lastName} onChange={handleLastNameChange} />
+        Last name:{" "}
+        <input
+          value={lastName}
+          onChange={handleLastNameChange}
+          className="my-1 rounded-md border border-gray-300 p-1"
+        />
       </label>
-      <p>
-        Your ticket will be issued to: <b>{fullName}</b>
+      <p className="mt-4 text-2xl">
+        Full Name: <b>{fullName}</b>
       </p>
-    </>
+    </div>
   );
 };
