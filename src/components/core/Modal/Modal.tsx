43e2091cf95ec1f49f5b96dd7637c46658bd4b94
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-09-19 13:09:09 -0600

    [136] Dialog

--- src/components/core/Modal/Modal.tsx
+++ src/components/core/Modal/Modal.tsx
@@ -1,57 +1,28 @@
-import { Dialog, Transition } from "@headlessui/react";
-import { Fragment, MutableRefObject } from "react";
+import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
 
 export type ModalProps = {
   isOpen: boolean;
   onClose: () => void;
   children: React.ReactNode;
-  initialFocus?: MutableRefObject<HTMLElement | null>;
 };
 
-export const Modal = ({
-  isOpen,
-  onClose,
-  initialFocus,
-  children,
-}: ModalProps) => {
+export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
   return (
-    <Transition.Root show={isOpen} as={Fragment}>
-      <Dialog
-        as="div"
-        className="relative z-10"
-        initialFocus={initialFocus}
-        onClose={onClose}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0">
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
+    <Dialog open={isOpen} onClose={onClose} className="relative z-10">
+      <DialogBackdrop
+        transition
+        className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+      />
 
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                {children}
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <DialogPanel
+            transition
+            className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95">
+            {children}
+          </DialogPanel>
         </div>
-      </Dialog>
-    </Transition.Root>
+      </div>
+    </Dialog>
   );
 };
-
-export const Title = Dialog.Title;
-export const Description = Dialog.Description;
