2afc7f8512fe54b175c64dc62d74b6000c37dd01
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-09-19 14:06:58 -0600

    [136] ComboBox

--- src/components/form/ComboBox/ComboBox.tsx
+++ src/components/form/ComboBox/ComboBox.tsx
@@ -6,7 +6,15 @@ import {
   ChevronUpDownIcon,
   ExclamationCircleIcon,
 } from "@heroicons/react/20/solid";
-import { Combobox } from "@headlessui/react";
+import {
+  Combobox,
+  ComboboxButton,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+  Field,
+  Label,
+} from "@headlessui/react";
 import { Option } from "@/types/core";
 
 type BaseConfig = { text: string };
@@ -80,14 +88,13 @@ export const ComboBox = forwardRef<HTMLInputElement, Props>(
       : filteredOptions;
 
     return (
-      <>
-        <Combobox as="div" value={value} onChange={onChange} nullable>
-          <Combobox.Label className="block text-sm font-medium leading-6 text-gray-900">
-            {label}{" "}
-            {showAsterisk && <span className="ml-1 text-red-700">*</span>}
-          </Combobox.Label>
+      <Field>
+        <Label className="block text-sm font-medium leading-6 text-gray-900">
+          {label} {showAsterisk && <span className="ml-1 text-red-700">*</span>}
+        </Label>
+        <Combobox as="div" value={value} onChange={onChange}>
           <div className="relative mt-2">
-            <Combobox.Input
+            <ComboboxInput
               className={clsx(
                 "w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",
                 error &&
@@ -99,7 +106,7 @@ export const ComboBox = forwardRef<HTMLInputElement, Props>(
               onChange={(event) => setQuery(event.target.value)}
               displayValue={(option: Option | null) => option?.label ?? ""}
             />
-            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+            <ComboboxButton className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
               {!error && (
                 <ChevronUpDownIcon
                   className="size-5 text-gray-400"
@@ -112,46 +119,26 @@ export const ComboBox = forwardRef<HTMLInputElement, Props>(
                   aria-hidden="true"
                 />
               )}
-            </Combobox.Button>
+            </ComboboxButton>
 
             {finalOptions.length > 0 && (
-              <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              <ComboboxOptions className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                 {finalOptions.map((option) => (
-                  <Combobox.Option
+                  <ComboboxOption
                     key={option.value}
                     value={option}
                     disabled={option.disabled}
-                    className={({ active, disabled }) =>
-                      clsx(
-                        "relative cursor-default select-none py-1.5 pl-3 pr-9",
-                        active ? "bg-indigo-600 text-white" : "text-gray-900",
-                        disabled && "cursor-not-allowed opacity-60",
-                      )
-                    }>
-                    {({ active, selected }) => (
-                      <>
-                        <span
-                          className={clsx(
-                            "block truncate",
-                            selected && "font-semibold",
-                          )}>
-                          {option.label}
-                        </span>
+                    className="relative cursor-default select-none py-1.5 pl-3 pr-9 text-gray-900 data-[disabled]:cursor-not-allowed data-[focus]:bg-indigo-600 data-[focus]:text-white data-[disabled]:opacity-60">
+                    <span className="block truncate data-[selected]:font-semibold">
+                      {option.label}
+                    </span>
 
-                        {selected && (
-                          <span
-                            className={clsx(
-                              "absolute inset-y-0 right-0 flex items-center pr-4",
-                              active ? "text-white" : "text-indigo-600",
-                            )}>
-                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                          </span>
-                        )}
-                      </>
-                    )}
-                  </Combobox.Option>
+                    <span className="absolute inset-y-0 left-0 hidden items-center pl-1.5 text-indigo-600 group-data-[selected]:flex group-data-[focus]:text-white">
+                      <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                    </span>
+                  </ComboboxOption>
                 ))}
-              </Combobox.Options>
+              </ComboboxOptions>
             )}
           </div>
         </Combobox>
@@ -163,7 +150,7 @@ export const ComboBox = forwardRef<HTMLInputElement, Props>(
             {error}
           </p>
         )}
-      </>
+      </Field>
     );
   },
 );
