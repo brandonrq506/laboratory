6951594801d7171891b52c80f2f9f07ce166fa14
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-10-14 22:27:47 -0600

    Comment

--- src/features/activities/api/axios/getActivities.ts
+++ src/features/activities/api/axios/getActivities.ts
@@ -1,6 +1,7 @@
 import { ACTIVITIES_ENDPOINT, USERS_ENDPOINT, apiV1 } from "@/libs/axios";
 import { GetActivity } from "../../types/getActivity";
 
+// TODO: Remove hardcoded user id
 const URL = `${USERS_ENDPOINT}/1${ACTIVITIES_ENDPOINT}`;
 
 type Props = { signal: AbortSignal };
