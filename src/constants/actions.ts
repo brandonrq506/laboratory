21e48a1adc085bd2b1dfdc3387c629c3beb4d9e3
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-10-19 11:07:05 -0600

    [170] Added Actions and Entities

---
+++ src/constants/actions.ts
@@ -0,0 +1,44 @@
+/* NAVIGATION ACTIONS */
+export const BACK = "Back";
+export const NEXT = "Next";
+export const PREVIOUS = "Previous";
+export const CONTINUE = "Continue";
+export const RETURN_HOME = "Return Home";
+
+/* CONFIRMATION ACTIONS */
+export const SEND = "Send";
+export const SAVE = "Save";
+export const UPDATE = "Update";
+export const SUBMIT = "Submit";
+export const IMPORT = "Import";
+export const CONFIRM = "Confirm";
+export const DOWNLOAD = "Download";
+
+/* CANCELLATION ACTIONS */
+export const CLOSE = "Close";
+export const CANCEL = "Cancel";
+
+/* DELETION ACTIONS */
+export const CLEAR = "Clear";
+export const REMOVE = "Remove";
+export const DELETE = "Delete";
+
+/* CREATION ACTIONS */
+export const ADD = "Add";
+export const CREATE = "Create";
+export const UPLOAD = "Upload";
+export const REGISTER = "Register";
+
+/* ACCOUNT MANAGEMENT ACTIONS */
+export const SIGN_IN = "Sign In";
+export const SIGN_OUT = "Sign Out";
+export const CREATE_ACCOUNT = "Create Account";
+
+/* REPAIRS */
+export const REPAIR = "Repair";
+export const GO_REPAIRS = "Go to Repairs";
+export const ADD_ANOTHER_REPAIR = "Add Another Repair";
+
+/* TOWS */
+export const GO_TOWS = "Go to Tows";
+export const ADD_ANOTHER_TOW = "Add Another Tow";
