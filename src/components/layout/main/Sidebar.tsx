547b695cc711b3a9c6dc18ba3c25defc289436f0
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-11-01 22:05:15 -0600

    Fix sidebar z index

--- src/components/layout/main/Sidebar.tsx
+++ src/components/layout/main/Sidebar.tsx
@@ -97,7 +97,7 @@ export const Sidebar = ({ isOpen, onClose }: SidebarProps) => {
       </Dialog>
 
       {/* Desktop Sidebar */}
-      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+      <div className="hidden lg:fixed lg:inset-y-0 lg:flex lg:w-72 lg:flex-col">
         {/* Sidebar component, swap this element with another sidebar if you like */}
         <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
           <div className="flex h-16 shrink-0 items-center">
