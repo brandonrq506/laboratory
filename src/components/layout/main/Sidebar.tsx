30b72cee31ee590a97d623904e16f97e17a901db
brandonrq506 <91104615+brandonrq506@users.noreply.github.com>
2024-11-01 22:03:43 -0600

    [187] Layout update sidebar options (#188)
    [187] Improved, Updated and Simplified Sidebar

--- src/components/layout/main/Sidebar.tsx
+++ src/components/layout/main/Sidebar.tsx
@@ -1,7 +1,9 @@
-import { Dialog, Transition } from "@headlessui/react";
-import { Fragment } from "react";
-import { NavLink } from "react-router-dom";
-import clsx from "clsx";
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  TransitionChild,
+} from "@headlessui/react";
 
 import {
   DocumentTextIcon,
@@ -10,13 +12,17 @@ import {
   UsersIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
+import { SidebarItem } from "./SidebarItem";
+
+const experiments = [
+  { name: "Projects", href: "/projects", icon: FolderIcon },
+  { name: "Modal Testing", href: "/modal-testing", icon: UsersIcon },
+  { name: "Form Projects", href: "/form-projects", icon: DocumentTextIcon },
+];
 
 const items = [
   { name: "Dashboard", href: "/", icon: HomeIcon },
-  { name: "Projects", href: "/projects", icon: FolderIcon },
   { name: "Activities", href: "/activities", icon: UsersIcon },
-  { name: "Modal Testing", href: "/modal-testing", icon: UsersIcon },
-  { name: "Form Projects", href: "/form-projects", icon: DocumentTextIcon },
 ];
 
 type SidebarProps = {
@@ -28,102 +34,77 @@ export const Sidebar = ({ isOpen, onClose }: SidebarProps) => {
   return (
     <>
       {/* Mobile Sidebar */}
-      <Transition.Root show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-50 lg:hidden" onClose={onClose}>
-          <Transition.Child
-            as={Fragment}
-            enter="transition-opacity ease-linear duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity ease-linear duration-300"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0">
-            <div className="fixed inset-0 bg-gray-900/80" />
-          </Transition.Child>
+      <Dialog
+        open={isOpen}
+        onClose={onClose}
+        className="relative z-50 lg:hidden">
+        <DialogBackdrop
+          transition
+          className="fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0"
+        />
 
-          <div className="fixed inset-0 flex">
-            <Transition.Child
-              as={Fragment}
-              enter="transition ease-in-out duration-300 transform"
-              enterFrom="-translate-x-full"
-              enterTo="translate-x-0"
-              leave="transition ease-in-out duration-300 transform"
-              leaveFrom="translate-x-0"
-              leaveTo="-translate-x-full">
-              <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
-                <Transition.Child
-                  as={Fragment}
-                  enter="ease-in-out duration-300"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="ease-in-out duration-300"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0">
-                  <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
-                    <button
-                      type="button"
-                      className="-m-2.5 p-2.5"
-                      onClick={onClose}>
-                      <span className="sr-only">Close sidebar</span>
-                      <XMarkIcon
-                        className="h-6 w-6 text-white"
-                        aria-hidden="true"
-                      />
-                    </button>
-                  </div>
-                </Transition.Child>
-                <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-2">
-                  <div className="flex h-16 shrink-0 items-center">
-                    <img
-                      className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                      alt="Your Company"
-                    />
-                  </div>
-                  <nav className="flex flex-1 flex-col">
-                    <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                      <li>
-                        <ul role="list" className="-mx-2 space-y-1">
-                          {items.map((item) => (
-                            <li key={item.name}>
-                              <NavLink
-                                to={item.href}
-                                onClick={onClose}
-                                className={({ isActive }) =>
-                                  clsx(
-                                    isActive
-                                      ? "bg-gray-50 text-indigo-600 *:text-indigo-600"
-                                      : "text-gray-700 *:text-gray-400 hover:bg-gray-50 hover:text-indigo-600",
-                                    "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6",
-                                  )
-                                }>
-                                <item.icon
-                                  className="h-6 w-6 shrink-0 group-hover:text-indigo-600"
-                                  aria-hidden="true"
-                                />
-                                {item.name}
-                              </NavLink>
-                            </li>
-                          ))}
-                        </ul>
-                      </li>
+        <div className="fixed inset-0 flex">
+          <DialogPanel
+            transition
+            className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-[closed]:-translate-x-full">
+            <TransitionChild>
+              <div className="absolute left-full top-0 flex w-16 justify-center pt-5 duration-300 ease-in-out data-[closed]:opacity-0">
+                <button
+                  type="button"
+                  onClick={onClose}
+                  className="-m-2.5 p-2.5">
+                  <span className="sr-only">Close sidebar</span>
+                  <XMarkIcon
+                    aria-hidden="true"
+                    className="h-6 w-6 text-white"
+                  />
+                </button>
+              </div>
+            </TransitionChild>
+            {/* Sidebar component, swap this element with another sidebar if you like */}
+            <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-2">
+              <div className="flex h-16 shrink-0 items-center">
+                <img
+                  alt="Your Company"
+                  src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                  className="h-8 w-auto"
+                />
+              </div>
+              <nav className="flex flex-1 flex-col">
+                <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                  <li>
+                    <ul role="list" className="-mx-2 space-y-1">
+                      {items.map((item) => (
+                        <SidebarItem {...item} onClose={onClose} />
+                      ))}
                     </ul>
-                  </nav>
-                </div>
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </Dialog>
-      </Transition.Root>
+                  </li>
+                  <li>
+                    <div className="text-xs/6 font-semibold text-gray-400">
+                      My experiments
+                    </div>
+                    <ul role="list" className="-mx-2 mt-2 space-y-1">
+                      {experiments.map((experiment) => (
+                        <SidebarItem {...experiment} onClose={onClose} />
+                      ))}
+                    </ul>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
 
       {/* Desktop Sidebar */}
-      <div className="hidden lg:fixed lg:inset-y-0 lg:flex lg:w-72 lg:flex-col">
+      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+        {/* Sidebar component, swap this element with another sidebar if you like */}
         <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
           <div className="flex h-16 shrink-0 items-center">
             <img
+              alt="Your Company"
+              src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
               className="h-8 w-auto"
-              src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
-              alt="The tailwind logo"
             />
           </div>
           <nav className="flex flex-1 flex-col">
@@ -131,38 +112,31 @@ export const Sidebar = ({ isOpen, onClose }: SidebarProps) => {
               <li>
                 <ul role="list" className="-mx-2 space-y-1">
                   {items.map((item) => (
-                    <li key={item.name}>
-                      <NavLink
-                        to={item.href}
-                        className={({ isActive }) =>
-                          clsx(
-                            isActive
-                              ? "bg-gray-50 text-indigo-600 *:text-indigo-600"
-                              : "text-gray-700 *:text-gray-400 hover:bg-gray-50 hover:text-indigo-600",
-                            "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6",
-                          )
-                        }>
-                        <item.icon
-                          className="h-6 w-6 shrink-0 group-hover:text-indigo-600"
-                          aria-hidden="true"
-                        />
-                        {item.name}
-                      </NavLink>
-                    </li>
+                    <SidebarItem {...item} />
+                  ))}
+                </ul>
+              </li>
+              <li>
+                <div className="text-xs/6 font-semibold text-gray-400">
+                  Your experiments
+                </div>
+                <ul role="list" className="-mx-2 mt-2 space-y-1">
+                  {experiments.map((experiment) => (
+                    <SidebarItem {...experiment} />
                   ))}
                 </ul>
               </li>
               <li className="-mx-6 mt-auto">
                 <a
                   href="#"
-                  className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-50">
+                  className="flex items-center gap-x-4 px-6 py-3 text-sm/6 font-semibold text-gray-900 hover:bg-gray-50">
                   <img
+                    alt=""
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                     className="h-8 w-8 rounded-full bg-gray-50"
-                    src="https://media.licdn.com/dms/image/C5103AQFaf0t2wKfA4A/profile-displayphoto-shrink_800_800/0/1521536559143?e=1709164800&v=beta&t=uiMNtPa-2jmt6Dk7Zg2RSLymcdqpzlTJZhg7m6PdCcM"
-                    alt="Photo of Brandon Ramirez"
                   />
-                  <span className="sr-only">Brandon's LinkedIn Profile</span>
-                  <span aria-hidden="true">Brandon Ramirez</span>
+                  <span className="sr-only">Your profile</span>
+                  <span aria-hidden="true">Tom Cook</span>
                 </a>
               </li>
             </ul>
