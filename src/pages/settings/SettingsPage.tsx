a33d23d6500094ddac406e5173c8897aa370812a
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-11-08 17:31:26 -0600

    [204] Created settings pages

---
+++ src/pages/settings/SettingsPage.tsx
@@ -0,0 +1,40 @@
+import { NavLink, Outlet } from "react-router-dom";
+import { Card } from "@/components/layout";
+import { clsx } from "clsx";
+
+const settingsPages = [
+  { name: "Account", href: "account" },
+  { name: "Categories", href: "categories" },
+  { name: "Notifications", href: "notifications" },
+];
+
+export const SettingsPage = () => {
+  return (
+    <Card className="h-full">
+      <header>
+        <nav>
+          <ul role="list" aria-label="Tabs" className="flex space-x-4">
+            {settingsPages.map((page) => (
+              <li>
+                <NavLink
+                  to={page.href}
+                  className={({ isActive }) =>
+                    clsx(
+                      isActive
+                        ? "bg-indigo-100 text-indigo-700"
+                        : "text-gray-500 hover:text-gray-700",
+                      "rounded-md px-3 py-2 text-sm font-medium",
+                    )
+                  }>
+                  {page.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+      <br />
+      <Outlet />
+    </Card>
+  );
+};
---
+++ src/pages/settings/index.ts
@@ -0,0 +1,4 @@
+export * from "./AccountSettingsPage";
+export * from "./CategorySettingsPage";
+export * from "./NotificationSettingsPage";
+export * from "./SettingsPage";
