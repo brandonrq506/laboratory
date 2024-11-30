365948bbc195a876a7217f28c08b379b0170f94b
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-11-29 21:21:39 -0600

    [223] Added new Dot component

--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -1,6 +1,7 @@
 export * from "./Button/Button";
 export * from "./Button/IconButton";
 export * from "./Button/LinkButton";
+export * from "./Dot/Dot";
 export * from "./Loading/Loading";
 export * from "./Modal/Modal";
 export * from "./Modal/ConfirmationModal";
