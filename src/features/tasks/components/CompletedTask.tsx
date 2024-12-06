d734db9a5ce235eb15cc243db0e0ac7694ff71f5
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-12-06 15:57:22 -0600

    [231] Added CompletedTask component

---
+++ src/features/tasks/components/CompletedTask.tsx
@@ -0,0 +1,41 @@
+import { Card } from "@/components/layout";
+import { CompletedTaskAPI } from "../types/completedTask";
+import { DeleteTask } from "./DeleteTask";
+import { Dot } from "@/components/core";
+
+import { convertSecondsToTime, formatDatetimeTo12hTime } from "@/utils";
+import { getColorByName } from "@/features/colors/utils/getColorByName";
+import { getDurationInSeconds } from "../utils/getDurationInSeconds";
+
+type Props = { task: CompletedTaskAPI };
+
+export const CompletedTask = ({ task }: Props) => {
+  const color = getColorByName(task.activity.category.color);
+  const durationSeconds = getDurationInSeconds(task.start_time, task.end_time);
+
+  return (
+    <Card className="flex justify-between shadow-sm">
+      <div className="flex flex-col justify-between">
+        <div className="flex items-center gap-1.5">
+          <Dot sizeStyles="size-2" colorStyles={color.fillClass} />
+          <p className="text-sm font-semibold">{task.activity.name}</p>
+          <p className="text-xs tabular-nums text-gray-600">
+            {convertSecondsToTime(durationSeconds)}
+          </p>
+        </div>
+
+        <div className="flex gap-1 text-xs text-gray-600">
+          <p className="tabular-nums">
+            {formatDatetimeTo12hTime(task.start_time)}
+          </p>
+          <p>{` — `}</p>
+          <p className="tabular-nums">
+            {formatDatetimeTo12hTime(task.end_time)}
+          </p>
+        </div>
+      </div>
+
+      <DeleteTask taskId={task.id} />
+    </Card>
+  );
+};
--- src/features/tasks/components/index.ts
+++ src/features/tasks/components/index.ts
@@ -1,3 +1,4 @@
+export * from "./CompletedTask";
 export * from "./CreateTaskForm";
 export * from "./ScheduledTask";
 export * from "./TaskList";
