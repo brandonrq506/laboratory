719205b576391dd9ca8a2ca09e55189b06fd35fc
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-12-06 15:58:23 -0600

    [231] Added extra component to TaskLists

--- src/pages/Tasks/TodayCompletedTaskList.tsx
+++ src/pages/Tasks/TodayCompletedTaskList.tsx
@@ -1,14 +1,14 @@
 import { useTasks } from "@/features/tasks/api/tanstack/useTasks";
 
-import { ScheduledTask, TaskList } from "@/features/tasks/components";
+import { CompletedTask, TaskList } from "@/features/tasks/components";
 import { Button } from "@/components/core";
-import { ScheduledTaskAPI } from "@/features/tasks/types/scheduledTask";
+import { CompletedTaskAPI } from "@/features/tasks/types/completedTask";
 
 const today = new Date().toISOString().split("T")[0];
 
-export const TodayScheduledTaskList = () => {
+export const TodayCompletedTaskList = () => {
   const { data, isPending, isError, refetch } = useTasks({
-    status: "scheduled",
+    status: "completed",
     created_at: today,
   });
 
@@ -19,7 +19,7 @@ export const TodayScheduledTaskList = () => {
   return (
     <TaskList
       tasks={data}
-      renderItem={(task) => <ScheduledTask task={task as ScheduledTaskAPI} />}
+      renderItem={(task) => <CompletedTask task={task as CompletedTaskAPI} />}
     />
   );
 };
