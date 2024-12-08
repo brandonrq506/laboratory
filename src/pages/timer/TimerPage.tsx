1fe964ccc262e12a383877e67e34326af04c66e1
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-12-07 21:23:41 -0600

    [246] Added Timer component

--- src/pages/timer/TimerPage.tsx
+++ src/pages/timer/TimerPage.tsx
@@ -1,13 +1,16 @@
-import { CreateTaskForm } from "@/features/tasks/components";
+import { Timer } from "./Timer";
 import { TodayCompletedTaskList } from "../Tasks/TodayCompletedTaskList";
 import { TodayScheduledTaskList } from "../Tasks/TodayScheduledTaskList";
 
 export const TimerPage = () => {
   return (
     <div>
-      <CreateTaskForm />
-      <TodayScheduledTaskList />
-      <TodayCompletedTaskList />
+      <Timer />
+      <br />
+      <div className="grid gap-5 md:grid-cols-2">
+        <TodayScheduledTaskList />
+        <TodayCompletedTaskList />
+      </div>
     </div>
   );
 };
