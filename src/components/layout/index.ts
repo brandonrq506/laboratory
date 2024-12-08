c8f3cd7bc73e52b5c5c4b64fbbaf708ac3d9ba5f
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-12-08 13:23:44 -0600

    [251] Added new Section Header

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -2,3 +2,4 @@ export * from "./main/MainLayout";
 export * from "./Card";
 export * from "./headings/PageHeader";
 export * from "./headings/PageHeaderWithActions";
+export * from "./headings/SectionHeaderWithAction";
