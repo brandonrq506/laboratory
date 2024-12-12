40b2f359d18e29957c2ece13c96452d441f29faa
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-12-11 20:00:03 -0600

    [260] Spotted a mistake and fixed it

--- src/utils/convertSecondsToHHMMSS.ts
+++ src/utils/convertSecondsToHHMMSS.ts
@@ -1,5 +1,5 @@
 const SECONDS_IN_HOUR = 3600;
-const MINUTES_IN_MINUTE = 60;
+const SECONDS_IN_MINUTE = 60;
 
 const padNumber = (number: number) => {
   const PADDING = 2;
@@ -10,8 +10,8 @@ export const convertSecondsToHHMMSS = (seconds: number) => {
   if (seconds < 0) throw new Error("Seconds must be a positive number");
 
   const hours = Math.floor(seconds / SECONDS_IN_HOUR);
-  const minutes = Math.floor((seconds % SECONDS_IN_HOUR) / MINUTES_IN_MINUTE);
-  const remainingSeconds = Math.floor(seconds % MINUTES_IN_MINUTE);
+  const minutes = Math.floor((seconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE);
+  const remainingSeconds = Math.floor(seconds % SECONDS_IN_MINUTE);
 
   return `${padNumber(hours)}:${padNumber(minutes)}:${padNumber(remainingSeconds)}`;
 };
