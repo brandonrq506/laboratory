8407e6d78644f4d5a3a771eac4daeb88dde32bee
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-12-11 19:57:46 -0600

    [260] new utils function to transform time to seconds

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export * from "./convertSecondsToHHMMSS";
 export * from "./convertSecondsToTime";
+export * from "./convertTimeToSeconds";
 export * from "./formatDatetimeTo12hTime";
 export * from "./transformToOption";
