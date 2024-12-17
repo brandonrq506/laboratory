fb7a2583615c3bd0539f9b5c1bd18b521fec1fa6
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-12-16 19:42:25 -0600

    [268] Created Activity Action Menu

--- src/features/activities/components/DeleteActivity.tsx
+++ src/features/activities/components/DeleteActivity.tsx
@@ -1,11 +1,11 @@
-import { useDeleteActivity } from "../api/tanstack/useDeleteActivity";
+import { useDisclosure } from "@/hooks";
 
-import { Button, ConfirmationModal, IconButton } from "@/components/core";
+import { DeleteActivityDialog } from "./DeleteActivityDialog";
+import { IconButton } from "@/components/core";
 import { TrashIcon } from "@heroicons/react/24/outline";
 
-import { CANCEL, CONFIRM, DELETE } from "@/constants/actions";
 import { ACTIVITY } from "@/constants/entities";
-import { useDisclosure } from "@/hooks";
+import { DELETE } from "@/constants/actions";
 
 type Props = {
   activityId: number;
@@ -14,16 +14,7 @@ type Props = {
 const BTN_TEXT = `${DELETE} ${ACTIVITY}`;
 
 export const DeleteActivity = ({ activityId }: Props) => {
-  const { mutate, isPending } = useDeleteActivity();
   const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const handleDelete = () => {
-    mutate(activityId, {
-      onSuccess: () => {
-        onClose();
-      },
-    });
-  };
 
   return (
     <>
@@ -35,29 +26,10 @@ export const DeleteActivity = ({ activityId }: Props) => {
         <TrashIcon className="size-5" aria-hidden />
       </IconButton>
 
-      <ConfirmationModal
+      <DeleteActivityDialog
         isOpen={isOpen}
         onClose={onClose}
-        icon="danger"
-        title="Delete Activity"
-        description="Are you sure you want to delete this activity?"
-        actions={
-          <>
-            <Button
-              variant="danger"
-              isLoading={isPending}
-              onClick={handleDelete}
-              className="inline-flex w-full justify-center sm:ml-3 sm:w-auto">
-              {CONFIRM}
-            </Button>
-            <Button
-              onClick={onClose}
-              variant="secondary"
-              className="mt-3 inline-flex w-full justify-center sm:mt-0 sm:w-auto">
-              {CANCEL}
-            </Button>
-          </>
-        }
+        activityId={activityId}
       />
     </>
   );
