d4573703ad98304de6d15f5ec2fc1d2c1ac91ab8
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-12-18 00:17:23 -0600

    [287] Made page a modal

--- src/pages/Tasks/NewTaskPage.tsx
+++ src/pages/Tasks/NewTaskPage.tsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+
+import { Modal } from "@/components/core";
+
 export const NewTaskPage = () => {
-  return <div>New Task</div>;
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => setIsOpen(true), []);
+
+  return (
+    <div>
+      <Modal isOpen={isOpen} onClose={() => navigate("..")}>
+        [287] This is a work in progress
+      </Modal>
+    </div>
+  );
 };
