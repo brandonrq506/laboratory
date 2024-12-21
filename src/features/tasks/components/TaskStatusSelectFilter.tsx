05e1eb98b474f1b8e5f24a4d754b734f8090504f
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-12-20 20:44:20 -0600

    [302] Used StringSelect to test

---
+++ src/features/tasks/components/TaskStatusSelectFilter.tsx
@@ -0,0 +1,52 @@
+import { useSearchParams } from "react-router";
+
+import { StringSelect } from "@/components/form";
+import { TaskStatus } from "../types/taskStatus";
+
+import { StringOption } from "@/types/core";
+
+type test = { value: TaskStatus; label: string };
+
+const status: test[] = [
+  { value: "scheduled", label: "Scheduled" },
+  { value: "in_progress", label: "In Progress" },
+  { value: "completed", label: "Completed" },
+];
+
+const defaultOptionValue = "all";
+const defaultOption: StringOption = { value: defaultOptionValue, label: "All" };
+
+export const TaskStatusSelectFilter = () => {
+  const [params, setParams] = useSearchParams({
+    status: "all",
+  });
+
+  const param = params.get("status") ?? defaultOptionValue;
+  const options = [defaultOption, ...status];
+
+  const selectedOption = options.find((o) => o.value === param) ?? null;
+
+  return (
+    <StringSelect
+      name="status"
+      label="Status"
+      options={options}
+      value={selectedOption}
+      onBlur={() => {}}
+      onChange={({ value }) => {
+        setParams(
+          (prev) => {
+            if (value === defaultOptionValue) {
+              prev.delete("status");
+              return prev;
+            }
+
+            prev.set("status", value);
+            return prev;
+          },
+          { replace: true },
+        );
+      }}
+    />
+  );
+};
--- src/features/tasks/components/index.ts
+++ src/features/tasks/components/index.ts
@@ -2,4 +2,5 @@ export * from "./CompletedTask";
 export * from "./CreateTaskForm";
 export * from "./ScheduledTask";
 export * from "./TaskList";
+export * from "./TaskStatusSelectFilter";
 export * from "./QuickCreateTaskMenu";
