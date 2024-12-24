cc740b998ee75fe55f937db2d8940e5e0b124648
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-12-23 19:38:57 -0600

    [317] Set-up msw

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,22 @@
 import "@testing-library/jest-dom";
+import { server } from "./server";
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
+
+const ResizeObserverMock = vi.fn(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}));
+
+vi.stubGlobal("ResizeObserver", ResizeObserverMock);
