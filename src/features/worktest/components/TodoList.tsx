f6489701fcec7e9c55b2f61bed556eee7d76d779
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-12-23 19:40:29 -0600

    [317] Added a couple tests for CategorySelect component

--- src/features/worktest/components/TodoList.tsx
+++ src/features/worktest/components/TodoList.tsx
@@ -1,11 +1,19 @@
+import { useForm } from "react-hook-form";
 import { useTodos } from "../api/useTodos";
+
+import { CategorySelect } from "@/features/categories/components";
 
 export const TodoList = () => {
   const { data, isSuccess } = useTodos();
-
+  const { control } = useForm({
+    defaultValues: { categories: { value: 1, label: "Productive" } },
+  });
   return (
     <div>
       <h1>{isSuccess && data.length}</h1>
+      <form>
+        <CategorySelect name="categories" control={control} />
+      </form>
     </div>
   );
 };
