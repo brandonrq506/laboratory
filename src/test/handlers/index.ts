5cdde42ecd96d4b59831cfab1b0c596d57427273
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2024-12-25 18:45:28 -0600

    [331] Added 3 new tests

--- src/test/handlers/index.ts
+++ src/test/handlers/index.ts
@@ -1,3 +1,4 @@
 import { activityHandlers } from "./activity";
+import { categoryHandlers } from "./category";
 
-export const handlers = [...activityHandlers];
+export const handlers = [...activityHandlers, ...categoryHandlers];
---
+++ src/test/store/categories.ts
@@ -0,0 +1,36 @@
+import { Category } from "@/features/categories/types/category";
+
+export const categories: Category[] = [
+  {
+    id: 1,
+    created_at: "2024-08-11T23:33:11.193Z",
+    name: "Productive",
+    updated_at: "2024-11-09T17:20:18.866Z",
+    user_id: 1,
+    color: "blue",
+  },
+  {
+    id: 2,
+    created_at: "2024-08-11T23:33:32.107Z",
+    name: "Wellness",
+    updated_at: "2024-11-09T05:39:37.605Z",
+    user_id: 1,
+    color: "emerald",
+  },
+  {
+    id: 3,
+    created_at: "2024-08-11T23:33:38.565Z",
+    name: "Necessary",
+    updated_at: "2024-11-28T06:19:29.358Z",
+    user_id: 1,
+    color: "amber",
+  },
+  {
+    id: 4,
+    created_at: "2024-08-11T23:33:58.056Z",
+    name: "Procrastination",
+    updated_at: "2024-11-09T05:39:51.571Z",
+    user_id: 1,
+    color: "rose",
+  },
+];
