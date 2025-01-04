ba9120ca23a4943ab4d7a762c3643a999754522e
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2025-01-03 20:29:25 -0600

    [359] New TimeInput component with timezone awareness

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,4 +2,5 @@ export * from "./convertSecondsToHHMMSS";
 export * from "./convertSecondsToTime";
 export * from "./convertTimeToSeconds";
 export * from "./formatDatetimeTo12hTime";
+export * from "./formatDatetimeTo24hTime";
 export * from "./transformToOption";
