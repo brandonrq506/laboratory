59c217c5b2718073e477d72718102b08d4341d12
brandonrq506 <91104615+brandonrq506@users.noreply.github.com>
2025-01-05 18:04:37 -0600

    [366] layout remove broken picture (#367)
    [366] LinkedIn svg and profile component

--- src/components/layout/main/MainLayout.tsx
+++ src/components/layout/main/MainLayout.tsx
@@ -1,9 +1,9 @@
 import { useDisclosure } from "@/hooks/useDisclosure";
 
-import { Link, Outlet } from "react-router";
+import { IconButton, LinkedInProfile } from "../../core";
 import { Bars3Icon } from "@heroicons/react/24/outline";
 import { Header } from "./Header";
-import { IconButton } from "../../core";
+import { Outlet } from "react-router";
 import { Sidebar } from "./Sidebar";
 
 export const MainLayout = () => {
@@ -22,17 +22,7 @@ export const MainLayout = () => {
         <div className="flex-1 text-sm font-semibold leading-6 text-gray-900">
           Laboratory
         </div>
-        <Link
-          to="https://www.linkedin.com/in/brandonrq506/"
-          target="_blank"
-          rel="noopener noreferrer">
-          <span className="sr-only">Brandon's LinkedIn Profile</span>
-          <img
-            className="h-8 w-8 rounded-full bg-gray-50"
-            src="https://media.licdn.com/dms/image/C5103AQFaf0t2wKfA4A/profile-displayphoto-shrink_800_800/0/1521536559143?e=1709164800&v=beta&t=uiMNtPa-2jmt6Dk7Zg2RSLymcdqpzlTJZhg7m6PdCcM"
-            alt="Photo of Brandon Ramirez"
-          />
-        </Link>
+        <LinkedInProfile />
       </Header>
 
       <main className="py-6 lg:pl-72">
