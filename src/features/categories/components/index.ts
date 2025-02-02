f1e07efb36542f47bc639512fe12872d8495ca34
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2025-02-02 15:17:34 -0600

    [452] Added Delete Cateogry components

--- src/features/categories/components/index.ts
+++ src/features/categories/components/index.ts
@@ -2,5 +2,6 @@ export * from "./CategoryBadge";
 export * from "./CategorySelect";
 export * from "./CategorySelectFilter";
 export * from "./CreateCategoryForm";
+export * from "./DeleteCategory";
 export * from "./EditCategoryForm";
 export * from "./NewCategoryButton";
