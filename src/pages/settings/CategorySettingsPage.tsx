2d574b035da65ce38321fa7df06ae3578a983287
Brandon Ramirez <brandon.ramirez@pernixlabs.com>
2025-02-06 22:35:01 -0600

    [473] Category badges

--- src/pages/settings/CategorySettingsPage.tsx
+++ src/pages/settings/CategorySettingsPage.tsx
@@ -1,60 +1,11 @@
 import { useCategories } from "@/features/categories/api/tanstack/useCategories";
 
 import {
-  DeleteCategory,
-  EditCategoryForm,
+  CategoryBadge,
   NewCategoryButton,
 } from "@/features/categories/components";
-import { Fragment } from "react";
 import { Outlet } from "react-router";
 import { PageHeaderWithActions } from "@/components/layout";
-
-import { Color } from "@/features/colors/types/color";
-import { clsx } from "clsx";
-import { getColorByName } from "@/features/colors/utils/getColorByName";
-
-// TODO: Extract to a separate file
-const DummyActivity = ({ color }: { color: Color }) => {
-  return (
-    <div
-      className={clsx(
-        "rounded-md border px-3 py-2",
-        color.bgClass,
-        color.textClass,
-        color.borderClass,
-      )}>
-      <div className="flex flex-col">
-        <p>Basketball</p>
-        <div className="flex gap-2 tracking-wide">
-          <p className="text-opacity-80 text-xs">Avg: 60m</p>
-          <p className="text-opacity-80 text-xs">Max: 120m</p>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-// TODO: Extract to a separate file
-const DummyTask = ({ color }: { color: Color }) => {
-  return (
-    <div
-      className={clsx(
-        "rounded-md border px-3 py-2",
-        color.bgClass,
-        color.textClass,
-        color.borderClass,
-      )}>
-      <div className="flex flex-col">
-        <p>Basketball</p>
-        <div className="flex gap-2 tracking-wide">
-          <p className="text-opacity-80 text-xs">Started: 15:24</p>
-          <p className="text-opacity-80 text-xs">Ended: 16:43</p>
-          <p className="text-opacity-80 text-xs">(01h:19m)</p>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 export const CategorySettingsPage = () => {
   const { data, isSuccess } = useCategories();
@@ -66,27 +17,17 @@ export const CategorySettingsPage = () => {
         className="mb-2"
         actions={<NewCategoryButton />}
       />
-      {isSuccess &&
-        data.map((category) => {
-          const colorObject = getColorByName(category.color);
 
-          return (
-            <Fragment key={category.id}>
-              <EditCategoryForm
-                categoryId={category.id}
-                initialValues={{
-                  name: category.name,
-                  color: { value: colorObject.id, label: colorObject.name },
-                }}
-              />
-              <DeleteCategory categoryId={category.id} />
-              <div className="mt-2 flex flex-col gap-2">
-                <DummyActivity color={colorObject} />
-                <DummyTask color={colorObject} />
+      <div className="flex flex-wrap items-center gap-2">
+        {isSuccess &&
+          data.map((category) => {
+            return (
+              <div key={category.id}>
+                <CategoryBadge category={category} />
               </div>
-            </Fragment>
-          );
-        })}
+            );
+          })}
+      </div>
 
       <Outlet />
     </div>
