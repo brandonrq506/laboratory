70a4aa1572ce780a1b2f00618ec38928bf475203
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-02-28 22:12:33 -0600

    [540] Improved Typing

--- src/features/tasks/types/editForm.ts
+++ src/features/tasks/types/editForm.ts
@@ -1,6 +1,9 @@
 import { CompletedTaskAPI } from "./completedTask";
 import { InProgressTaskAPI } from "./inProgressTast";
+import { Option } from "@/types/core";
 
 type EditableTask = CompletedTaskAPI | InProgressTaskAPI;
 
-export type EditForm = Pick<EditableTask, "end_time" | "start_time">;
+export type EditForm = Pick<EditableTask, "start_time" | "end_time"> & {
+  activity: Option;
+};
