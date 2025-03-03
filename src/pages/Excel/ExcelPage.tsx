fd73ec098b2427b83b06b3f5874c97c86674653d
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-03-02 22:32:12 -0600

    [515] Built ExcelPage

---
+++ src/pages/Excel/ExcelPage.tsx
@@ -0,0 +1,13 @@
+import { DateFilter } from "@/components/core/Date";
+import { ExcelPageContent } from "./ExcelPageContent";
+import { PageHeader } from "@/components/layout";
+
+export const ExcelPage = () => {
+  return (
+    <div>
+      <PageHeader title="Excel Exporter" />
+      <DateFilter label="Date" className="sm:w-1/2" />
+      <ExcelPageContent />
+    </div>
+  );
+};
