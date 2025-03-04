76dd46cf6b96e443166d9cc90a5b15b9af621188
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-03-03 18:03:02 -0600

    [548] Split DateFilter into DateInput and DateFilter

--- src/components/core/Date/DateFilter.spec.tsx
+++ src/components/core/Date/DateFilter.spec.tsx
@@ -1,84 +1,43 @@
 import { render, screen } from "@testing-library/react";
-import { DateFilter } from "./DateFilter";
-import { createRoutesStub } from "react-router";
-import userEvent from "@testing-library/user-event";
+import { DateInput } from "./DateInput";
 
-describe("DateFilter", () => {
+describe("DateInput", () => {
   it("renders the label and input correctly", () => {
-    const Stub = createRoutesStub([
-      { path: "/", Component: () => <DateFilter label="Date" /> },
-    ]);
-    render(<Stub initialEntries={["/"]} />);
+    render(<DateInput label="Date" />);
 
     expect(screen.getByLabelText(/Date/i)).toBeInTheDocument();
   });
 
   it("displays the asterisk if showAsterisk is true", () => {
-    const Stub = createRoutesStub([
-      { path: "/", Component: () => <DateFilter label="Date" showAsterisk /> },
-    ]);
-    render(<Stub initialEntries={["/"]} />);
+    render(<DateInput label="Date" showAsterisk />);
 
     expect(screen.getByText("*")).toBeInTheDocument();
   });
 
   it("does not display the asterisk if showAsterisk is false", () => {
-    const Stub = createRoutesStub([
-      { path: "/", Component: () => <DateFilter label="Date" /> },
-    ]);
-    render(<Stub initialEntries={["/"]} />);
+    render(<DateInput label="Date" />);
 
     expect(screen.queryByText("*")).not.toBeInTheDocument();
   });
 
   it("renders a description if provided", () => {
-    const Stub = createRoutesStub([
-      {
-        path: "/",
-        Component: () => (
-          <DateFilter label="Date" description="Select a date" />
-        ),
-      },
-    ]);
-    render(<Stub initialEntries={["/"]} />);
+    render(<DateInput label="Date" description="Select a date" />);
+
     expect(screen.getByText(/Select a date/i)).toBeInTheDocument();
   });
 
   it("hides the label if hideLabel is true", () => {
-    const Stub = createRoutesStub([
-      { path: "/", Component: () => <DateFilter label="Date" hideLabel /> },
-    ]);
-    render(<Stub initialEntries={["/"]} />);
+    render(<DateInput label="Date" hideLabel />);
 
     expect(screen.getByText(/Date/i)).toHaveClass("sr-only");
   });
 
   it("renders the input with the correct value", () => {
-    const Stub = createRoutesStub([
-      { path: "/", Component: () => <DateFilter label="Date" /> },
-    ]);
-    render(<Stub initialEntries={["/"]} />);
+    const value = "2021-01-01";
+    render(<DateInput label="Date" value={value} onChange={() => {}} />);
 
     const input = screen.getByLabelText(/Date/i) as HTMLInputElement;
 
-    const now = new Date();
-    now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
-    const today = now.toISOString().split("T")[0];
-
-    expect(input.value).toBe(today);
-  });
-
-  it("updates search params when input value changes", async () => {
-    const Stub = createRoutesStub([
-      { path: "/", Component: () => <DateFilter label="Date" /> },
-    ]);
-    render(<Stub initialEntries={["/"]} />);
-    const input = screen.getByLabelText(/Date/i) as HTMLInputElement;
-
-    const newDate = "2025-01-01";
-    await userEvent.clear(input);
-    await userEvent.type(input, newDate);
-
-    expect(input.value).toBe(newDate);
+    expect(input.value).toBe(value);
   });
 });
