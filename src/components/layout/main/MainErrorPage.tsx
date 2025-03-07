5b04a84894c79e93edd81122675b235937842608
brandonrq506 <91104615+brandonrq506@users.noreply.github.com>
2025-03-07 10:38:14 -0600

    Improving error catching (#588)
    Improving error catching

--- src/components/layout/main/MainErrorPage.tsx
+++ src/components/layout/main/MainErrorPage.tsx
@@ -29,27 +29,32 @@ export const MainErrorPage = () => {
         </div>
       </main>
     );
-  }
-  return (
-    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="text-center">
-        <p className="text-base font-semibold text-indigo-600">Oops!</p>
-        <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-          Something went wrong
-        </h1>
-        <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-          This issue will be reported to our support team. Please try again
-          later.
-        </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <LinkButton
-            to="/"
-            size="xl"
-            startIcon={<HomeIcon aria-hidden className="size-5" />}>
-            Go back home
-          </LinkButton>
+  } else if (error instanceof Error) {
+    return (
+      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-indigo-600">Oops!</p>
+          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+            {error.message}
+          </h1>
+          <p>
+            <pre>{error.stack}</pre>
+          </p>
+          <p>{error.name}</p>
+          <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+            This issue will be reported to our support team. Please try again
+            later.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <LinkButton
+              to="/"
+              size="xl"
+              startIcon={<HomeIcon aria-hidden className="size-5" />}>
+              Go back home
+            </LinkButton>
+          </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 };
