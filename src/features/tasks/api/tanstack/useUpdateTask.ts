c636f26658466ad193735205c2d960e2c39a4d4d
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-03-11 21:03:03 -0600

    [552] First optimistic Update

--- src/features/tasks/api/tanstack/useUpdateTask.ts
+++ src/features/tasks/api/tanstack/useUpdateTask.ts
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
+import { BaseTaskAPI } from "../../types/baseTask";
 import { taskKeys } from "../queryKeys";
 import { updateTask } from "../axios/updateTask";
 
@@ -8,7 +9,33 @@ export const useUpdateTask = () => {
 
   return useMutation({
     mutationFn: updateTask,
-    onSuccess: () => {
+    onMutate: async ({ task, taskId }) => {
+      await queryClient.cancelQueries({ queryKey: taskKeys.lists() });
+
+      const previousTaskQueries = queryClient.getQueriesData({
+        queryKey: taskKeys.lists(),
+      });
+
+      queryClient.setQueriesData(
+        { queryKey: taskKeys.lists() },
+        (oldTasks: BaseTaskAPI[]) => {
+          return oldTasks.map((oldTask) =>
+            oldTask.id === taskId ? { ...oldTask, ...task } : oldTask,
+          );
+        },
+      );
+
+      return { previousTaskQueries };
+    },
+    onError: (_, __, context) => {
+      if (context?.previousTaskQueries) {
+        context.previousTaskQueries.forEach(([queryKey, previousData]) => {
+          queryClient.setQueryData(queryKey, previousData);
+        });
+      }
+    },
+
+    onSettled: () => {
       queryClient.invalidateQueries({ queryKey: taskKeys.all });
     },
   });
