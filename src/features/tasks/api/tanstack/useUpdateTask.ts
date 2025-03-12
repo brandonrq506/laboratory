b541a5eb820ef96cc1b458ff30c1887b4eed051e
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-03-11 21:50:23 -0600

    [552] Made core more concise and clear

--- src/features/tasks/api/tanstack/useUpdateTask.ts
+++ src/features/tasks/api/tanstack/useUpdateTask.ts
@@ -10,27 +10,24 @@ export const useUpdateTask = () => {
   return useMutation({
     mutationFn: updateTask,
     onMutate: async ({ task, taskId }) => {
-      await queryClient.cancelQueries({ queryKey: taskKeys.lists() });
+      const queryKey = { queryKey: taskKeys.lists() };
 
-      const previousTaskQueries = queryClient.getQueriesData({
-        queryKey: taskKeys.lists(),
+      await queryClient.cancelQueries(queryKey);
+
+      const prevQueries = queryClient.getQueriesData(queryKey);
+
+      queryClient.setQueriesData(queryKey, (oldTasks: BaseTaskAPI[]) => {
+        return oldTasks.map((oldTask) =>
+          oldTask.id === taskId ? { ...oldTask, ...task } : oldTask,
+        );
       });
 
-      queryClient.setQueriesData(
-        { queryKey: taskKeys.lists() },
-        (oldTasks: BaseTaskAPI[]) => {
-          return oldTasks.map((oldTask) =>
-            oldTask.id === taskId ? { ...oldTask, ...task } : oldTask,
-          );
-        },
-      );
-
-      return { previousTaskQueries };
+      return { prevQueries };
     },
     onError: (_, __, context) => {
-      if (context?.previousTaskQueries) {
-        context.previousTaskQueries.forEach(([queryKey, previousData]) => {
-          queryClient.setQueryData(queryKey, previousData);
+      if (context?.prevQueries) {
+        context.prevQueries.forEach(([queryKey, prevData]) => {
+          queryClient.setQueryData(queryKey, prevData);
         });
       }
     },
