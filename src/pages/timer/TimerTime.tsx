57f62259050280affe9d6f0d49a050387b778c90
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-03-17 22:23:42 -0600

    [613] Added new fix

--- src/pages/timer/TimerTime.tsx
+++ src/pages/timer/TimerTime.tsx
@@ -8,8 +8,8 @@ const MILI_TO_SEC = 1000;
 const elapsedTime = (start_time: string) => {
   const start = new Date(start_time);
   const now = new Date();
-  const elapsed = now.getTime() - start.getTime();
-  return elapsed / MILI_TO_SEC;
+  const elapsed = (now.getTime() - start.getTime()) / MILI_TO_SEC;
+  return Math.max(0, elapsed);
 };
 
 type Props = {
