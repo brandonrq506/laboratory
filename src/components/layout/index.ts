d3677ce3a55b875c0aa7f43f2f2ecd62c4840244
brandonrq506 <91104615+brandonrq506@users.noreply.github.com>
2025-03-19 17:18:00 -0600

    [631] Task remove empty button (#634)
    [631] Task remove empty button

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -3,5 +3,6 @@ export * from "./main/MainLayout";
 export * from "./Card";
 export * from "./headings/PageHeader";
 export * from "./headings/PageHeaderWithActions";
+export * from "./headings/SectionHeader";
 export * from "./headings/SectionHeaderWithAction";
 export * from "./SectionUnderConstruction";
--- src/pages/Tasks/TodayCompletedTaskList.tsx
+++ src/pages/Tasks/TodayCompletedTaskList.tsx
@@ -2,11 +2,10 @@ import { usePrefetchTask } from "@/features/tasks/api/tanstack/usePrefetchTask";
 import { useTasks } from "@/features/tasks/api/tanstack/useTasks";
 
 import { CompletedTask, TaskList } from "@/features/tasks/components";
-import { IconButton, Loading } from "@/components/core";
 import { CompletedTaskAPI } from "@/features/tasks/types/completedTask";
 import { Link } from "react-router";
-import { PlusIcon } from "@heroicons/react/24/outline";
-import { SectionHeaderWithAction } from "@/components/layout";
+import { Loading } from "@/components/core";
+import { SectionHeader } from "@/components/layout";
 import { TaskErrorList } from "@/features/tasks/components/TaskErrorList";
 
 export const TodayCompletedTaskList = () => {
@@ -19,15 +18,7 @@ export const TodayCompletedTaskList = () => {
   if (isPending)
     return (
       <div>
-        <SectionHeaderWithAction
-          title="Today's Completed Tasks"
-          className="pr-2.5"
-          action={
-            <IconButton shape="circle">
-              <PlusIcon className="size-5" aria-hidden />
-            </IconButton>
-          }
-        />
+        <SectionHeader title="Today's Completed Tasks" className="pr-2.5" />
         <Loading className="mx-auto my-10" sizeStyles="size-10" />
       </div>
     );
@@ -36,15 +27,7 @@ export const TodayCompletedTaskList = () => {
 
   return (
     <div>
-      <SectionHeaderWithAction
-        title="Today's Completed Tasks"
-        className="pr-2.5"
-        action={
-          <IconButton shape="circle">
-            <PlusIcon className="size-5" aria-hidden />
-          </IconButton>
-        }
-      />
+      <SectionHeader title="Today's Completed Tasks" className="pr-2.5" />
       <TaskList
         tasks={data}
         renderItem={(task) => (
