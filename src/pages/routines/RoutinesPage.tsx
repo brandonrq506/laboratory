afc6a50102be344c0b15cdf49d87ae3a1025cd1c
brandonrq506 <91104615+brandonrq506@users.noreply.github.com>
2025-04-09 16:32:55 -0600

    [689] Add option to remove Routines (#690)
    [689] Add option to remove Routines

--- src/pages/routines/RoutinesPage.tsx
+++ src/pages/routines/RoutinesPage.tsx
@@ -1,21 +1,44 @@
+import { useDeleteRoutine } from "@/features/routines/api/tanstack/useDeleteRoutine";
 import { useRoutines } from "@/features/routines/api/tanstack/useRoutines";
 
-import { PageHeader } from "@/components/layout";
+import { Card, PageHeader, SectionHeaderWithAction } from "@/components/layout";
+import { IconButton } from "@/components/core";
+import { TrashIcon } from "@heroicons/react/24/outline";
+
+import { DELETE } from "@/constants/actions";
+import { ROUTINE } from "@/constants/entities";
 
 export const RoutinesPage = () => {
   const { data, isSuccess } = useRoutines();
+  const { mutate } = useDeleteRoutine();
+
+  const deleteText = `${DELETE} ${ROUTINE}`;
+
+  const onDeleteRoutine = (id: number) => mutate(id);
 
   return (
     <div>
-      <PageHeader title="Routines" />
+      <PageHeader title="Routines" className="mb-2" />
 
       <div className="flex flex-wrap items-center gap-2">
         {isSuccess &&
           data.map((routine) => {
             return (
-              <div key={routine.id} className="rounded border p-4">
-                <h3>{routine.name}</h3>
-              </div>
+              <Card key={routine.id} className="w-full sm:w-1/2 lg:w-1/3">
+                <SectionHeaderWithAction
+                  title={routine.name}
+                  className="gap-x-2"
+                  action={
+                    <IconButton
+                      shape="circle"
+                      aria-label={deleteText}
+                      variant="dangerOutline"
+                      onClick={() => onDeleteRoutine(routine.id)}>
+                      <TrashIcon className="size-5" aria-hidden />
+                    </IconButton>
+                  }
+                />
+              </Card>
             );
           })}
       </div>
