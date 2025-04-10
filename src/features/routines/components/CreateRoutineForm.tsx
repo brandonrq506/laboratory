08346a7e57e49166c62c18542574ddbbefb8652e
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-04-10 12:17:57 -0600

    [688] Added Routine form

---
+++ src/features/routines/components/CreateRoutineForm.tsx
@@ -0,0 +1,22 @@
+import { useCreateRoutine } from "../api/tanstack/useCreateRoutine";
+import { useNavigate } from "react-router";
+
+import { ADD } from "@/constants/actions";
+import { ROUTINE } from "@/constants/entities";
+import { RoutineForm } from "./RoutineForm";
+
+import { RoutineForm as RoutineFormType } from "../types/routineForm";
+
+export const CreateRoutineForm = () => {
+  const navigate = useNavigate();
+  const { mutateAsync } = useCreateRoutine();
+
+  const onSubmit = async (values: RoutineFormType) => {
+    await mutateAsync({ name: values.name });
+    navigate("..");
+  };
+
+  return (
+    <RoutineForm submitButtonText={`${ADD} ${ROUTINE}`} onSubmit={onSubmit} />
+  );
+};
