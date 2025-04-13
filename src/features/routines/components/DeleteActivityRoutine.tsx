32ec64e1b80a5a094f07c17a81732a0fdf9612e3
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-04-13 00:45:35 -0600

    [703] Delete endpoint and component

---
+++ src/features/routines/components/DeleteActivityRoutine.tsx
@@ -0,0 +1,30 @@
+import { useDeleteActivityRoutine } from "../api/tanstack/useDeleteActivityRoutine";
+
+import { IconButton, Loading } from "@/components/core";
+import { TrashIcon } from "@heroicons/react/24/outline";
+
+import { ACTIVITY, ROUTINE } from "@/constants/entities";
+import { DELETE } from "@/constants/actions";
+
+interface Props {
+  activityId: number;
+  routineId: number;
+}
+
+const trashIcon = <TrashIcon className="size-5" aria-hidden />;
+const loadingIcon = <Loading className="size-5" />;
+const BTN_TEXT = `${DELETE} ${ACTIVITY} ${ROUTINE}`;
+
+export const DeleteActivityRoutine = ({ activityId, routineId }: Props) => {
+  const { mutate, isPending } = useDeleteActivityRoutine();
+
+  return (
+    <IconButton
+      shape="circle"
+      aria-label={BTN_TEXT}
+      variant="dangerOutline"
+      onClick={() => mutate({ activityId, routineId })}>
+      {isPending ? loadingIcon : trashIcon}
+    </IconButton>
+  );
+};
