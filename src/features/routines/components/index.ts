c0d390aa7e6d0cc8801627dcbe6d5a122f7d04d1
brandonrq506 <91104615+brandonrq506@users.noreply.github.com>
2025-04-13 01:09:12 -0600

    [715] Moved PlayRoutine to show page (#716)
    [715] Moved PlayRoutine to show page

--- src/features/routines/components/index.ts
+++ src/features/routines/components/index.ts
@@ -1,4 +1,5 @@
 export * from "./DeleteActivityRoutine";
 export * from "./DeleteRoutineDialog";
+export * from "./PlayRoutineButton";
 export * from "./RoutineActivityList";
 export * from "./RoutineNameForm";
--- src/pages/routines/RoutinesPage.tsx
+++ src/pages/routines/RoutinesPage.tsx
@@ -1,29 +1,22 @@
-import { useApplyRoutine } from "@/features/routines/api/tanstack/useApplyRoutine";
 import { useRoutines } from "@/features/routines/api/tanstack/useRoutines";
 
+import { Badge, LinkButton } from "@/components/core";
 import {
   Card,
   PageHeaderWithActions,
   SectionHeaderWithAction,
 } from "@/components/layout";
-import { IconButton, LinkButton } from "@/components/core";
 import { Link, Outlet } from "react-router";
 
-import { PlayIcon } from "@heroicons/react/24/solid";
 import { PlusIcon } from "@heroicons/react/24/outline";
 
-import { ADD, DELETE } from "@/constants/actions";
+import { ADD } from "@/constants/actions";
 import { ROUTINE } from "@/constants/entities";
 
 import { convertSecondsToTime } from "@/utils";
 
 export const RoutinesPage = () => {
   const { data, isSuccess } = useRoutines();
-  const { mutate: applyMutate } = useApplyRoutine();
-
-  const deleteText = `${DELETE} ${ROUTINE}`;
-
-  const onApplyRoutine = (id: number) => applyMutate(id);
 
   const totalAvgTime = (activities: { activity_avg_time: number }[]) => {
     return activities.reduce((total, activity) => {
@@ -51,27 +44,23 @@ export const RoutinesPage = () => {
           data.map((routine) => {
             return (
               <Card key={routine.id} className="w-full sm:w-1/2 lg:w-1/3">
-                <SectionHeaderWithAction
-                  title={`${routine.name} ${convertSecondsToTime(totalAvgTime(routine.activities))}`}
-                  className="gap-x-2"
-                  action={
-                    <IconButton
-                      shape="circle"
-                      aria-label={deleteText}
-                      variant="primaryOutline"
-                      onClick={() => onApplyRoutine(routine.id)}>
-                      <PlayIcon className="size-5" aria-hidden />
-                    </IconButton>
-                  }
-                />
-
                 <Link to={`edit/${routine.id}`}>
+                  <SectionHeaderWithAction
+                    title={routine.name}
+                    className="gap-x-2"
+                    action={convertSecondsToTime(
+                      totalAvgTime(routine.activities),
+                    )}
+                  />
+
                   {routine.activities.map((activity) => (
-                    <div key={activity.id} className="flex justify-between">
+                    <div
+                      key={activity.id}
+                      className="my-1 flex justify-between">
                       <div className="flex items-center gap-x-2">
-                        <span className="text-sm font-medium">
+                        <Badge color={activity.category_color}>
                           {activity.activity_name}
-                        </span>
+                        </Badge>
                         <span className="text-xs text-gray-500">
                           {activity.category_name}
                         </span>
