2344079dcd0b0a19c26743d856446d7182f1629f
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-04-26 13:54:39 -0600

    [759] Added History and Excel links

--- src/pages/Excel/ExcelPage.tsx
+++ src/pages/Excel/ExcelPage.tsx
@@ -1,11 +1,20 @@
+import { AdminProtectedContent } from "@/features/user/components";
 import { DateFilter } from "@/components/core/Date";
 import { ExcelPageContent } from "./ExcelPageContent";
-import { PageHeader } from "@/components/layout";
+import { HistoryLink } from "./HistoryLink";
+import { PageHeaderWithActions } from "@/components/layout";
 
 export const ExcelPage = () => {
   return (
     <div>
-      <PageHeader title="Excel Exporter" />
+      <PageHeaderWithActions
+        title="Excel Exporter"
+        actions={
+          <AdminProtectedContent>
+            <HistoryLink />
+          </AdminProtectedContent>
+        }
+      />
       <DateFilter label="Date" hideLabel className="sm:w-1/2" />
       <ExcelPageContent />
     </div>
--- src/pages/Tasks/HistoryPage.tsx
+++ src/pages/Tasks/HistoryPage.tsx
@@ -1,12 +1,21 @@
+import { AdminProtectedContent } from "@/features/user/components";
 import { DateFilter } from "@/components/core/Date";
+import { ExcelLink } from "./ExcelLink";
 import { FilterableTaskList } from "./FilterableTaskList";
 import { Outlet } from "react-router";
-import { PageHeader } from "@/components/layout";
+import { PageHeaderWithActions } from "@/components/layout";
 
 export const HistoryPage = () => {
   return (
     <div>
-      <PageHeader title="Tasks" />
+      <PageHeaderWithActions
+        title="Tasks"
+        actions={
+          <AdminProtectedContent>
+            <ExcelLink />
+          </AdminProtectedContent>
+        }
+      />
       <div className="mb-2 w-2/3 md:w-1/3">
         <DateFilter label="Date Filter" hideLabel className="w-full" />
       </div>
