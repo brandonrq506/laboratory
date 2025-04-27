cdbdfe4ecf92265dccfe0f76c5c93c99a99b8784
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-04-27 10:35:26 -0600

    Revert "[676] Modal Shows up from the top (#780)"
    This reverts commit 95b674774274eebb7db4380a882ba1dc9ded3c2a.

--- src/components/core/Modal/Modal.tsx
+++ src/components/core/Modal/Modal.tsx
@@ -15,10 +15,10 @@ export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
       />
 
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex min-h-full items-start justify-center p-4 text-center sm:items-center sm:p-0">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <DialogPanel
             transition
-            className="relative w-full max-w-md transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all data-closed:-translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:max-w-lg sm:p-6 sm:data-closed:translate-y-0 sm:data-closed:scale-95">
+            className="relative w-full max-w-md transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:max-w-lg sm:p-6 sm:data-closed:translate-y-0 sm:data-closed:scale-95">
             {children}
           </DialogPanel>
         </div>
