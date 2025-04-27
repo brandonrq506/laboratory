d09a698f2bf075105e7ed51cad00bcdd94233718
brandonrq506 <91104615+brandonrq506@users.noreply.github.com>
2025-04-27 12:05:28 -0600

    [781] Modals are keyboard aware now (#782)
    [781] Modals are keyboard aware now

--- src/components/core/Modal/Modal.tsx
+++ src/components/core/Modal/Modal.tsx
@@ -1,4 +1,7 @@
+import { useKeywordVisible } from "@/hooks";
+
 import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
+import clsx from "clsx";
 
 export type ModalProps = {
   isOpen: boolean;
@@ -7,6 +10,8 @@ export type ModalProps = {
 };
 
 export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
+  const isKeyboardVisible = useKeywordVisible();
+
   return (
     <Dialog open={isOpen} onClose={onClose} className="relative z-10">
       <DialogBackdrop
@@ -15,7 +20,11 @@ export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
       />
 
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+        <div
+          className={clsx(
+            "flex min-h-full justify-center p-4 text-center sm:items-center sm:p-0",
+            isKeyboardVisible ? "items-start" : "items-end",
+          )}>
           <DialogPanel
             transition
             className="relative w-full max-w-md transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:max-w-lg sm:p-6 sm:data-closed:translate-y-0 sm:data-closed:scale-95">
---
+++ src/hooks/useKeyboardVisible.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+// 100px is a heuristic
+const KEYBOARD_HEIGHT = 100;
+
+export const useKeywordVisible = () => {
+  const [isKeyboardVisible, setIsKeyboardVisible] = useState(false);
+
+  useEffect(() => {
+    const viewport = window.visualViewport;
+
+    if (!viewport) return;
+
+    const handleResize = () => {
+      const heightDiff = window.innerHeight - viewport.height;
+
+      setIsKeyboardVisible(heightDiff > KEYBOARD_HEIGHT);
+    };
+
+    viewport.addEventListener("resize", handleResize);
+
+    return () => viewport.removeEventListener("resize", handleResize);
+  }, []);
+
+  return isKeyboardVisible;
+};
