caa030b9bfddb50e250606a852214aac1ed2bc3c
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-04-30 16:48:57 -0600

    [799] Added Routine handlers and stores

--- src/test/handlers/index.ts
+++ src/test/handlers/index.ts
@@ -1,4 +1,11 @@
 import { activityHandlers } from "./activity";
+import { activityRoutineHandlers } from "./activityRoutine";
 import { categoryHandlers } from "./category";
+import { routineHandlers } from "./routine";
 
-export const handlers = [...activityHandlers, ...categoryHandlers];
+export const handlers = [
+  ...activityHandlers,
+  ...activityRoutineHandlers,
+  ...categoryHandlers,
+  ...routineHandlers,
+];
