7cb3f546384d16fcf610a0aa04c804016e0ce7ae
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-05-02 12:05:51 -0600

    [820] Hide Error message from Timer

---
+++ src/pages/timer/tests/IdleTimer.spec.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from "@/test/test-utils";
+import userEvent from "@testing-library/user-event";
+
+import { IdleTimer } from "../IdleTimer";
+
+describe("IdleTimer", () => {
+  it("should be focused on the activity input when the component is mounted", async () => {
+    const user = userEvent.setup();
+    render(<IdleTimer />);
+
+    const input = screen.getByRole("combobox", { name: "Activities" });
+
+    expect(input).toHaveFocus();
+
+    await user.type(input, "Test");
+
+    expect(input).toHaveValue("Test");
+  });
+
+  it("should display error status when activity is not selected", async () => {
+    const user = userEvent.setup();
+    render(<IdleTimer />);
+
+    const input = screen.getByRole("combobox", { name: "Activities" });
+    const playBtn = screen.getByRole("button", { name: "Play Button" });
+
+    await user.click(playBtn);
+
+    expect(input).toHaveAttribute("aria-invalid");
+    expect(input).toHaveClass("ring-red-300");
+    expect(screen.queryByRole("alert")).not.toBeInTheDocument();
+  });
+
+  // it("should be disabled when the form is submitting", async () => {});
+
+  // it("must not allow to submit when the form is submitting", async () => {});
+
+  // it("displays a loading state when the form is submitting", async () => {});
+
+  // it("displays a retry button when the form is submitting", async () => {});
+
+  // it("should call the onSubmit function when the form is submitted", async () => {});
+
+  // it("should be disabled when the user is offline", async () => {});
+});
