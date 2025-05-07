ddea4a58b36fbebd721d1189ebe09a5cfa9234d5
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-05-07 15:04:01 -0600

    [848] Implement optimistic update

--- src/features/tasks/api/tanstack/useStartTask.ts
+++ src/features/tasks/api/tanstack/useStartTask.ts
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
+import { inProgressTaskOptions, scheduledTasksOptions } from "../queryOptions";
 import { startTask } from "../axios/startTask";
 import { taskKeys } from "../queryKeys";
 
@@ -8,8 +9,48 @@ export const useStartTask = () => {
 
   return useMutation({
     mutationFn: startTask,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: taskKeys.lists() });
+    onMutate: async (newInProgressTask) => {
+      await queryClient.cancelQueries({ queryKey: taskKeys.lists() });
+
+      const prevScheduled = queryClient.getQueryData(
+        scheduledTasksOptions().queryKey,
+      );
+
+      // Add new in_progress to in_progress tasks cache
+      queryClient.setQueryData(inProgressTaskOptions().queryKey, [
+        newInProgressTask,
+      ]);
+
+      // Remove new in_progress from the scheduled tasks cache
+      queryClient.setQueryData(
+        scheduledTasksOptions().queryKey,
+        (previousTasks) => {
+          if (previousTasks) {
+            return previousTasks.filter(
+              (task) => task.id !== newInProgressTask.id,
+            );
+          }
+          return [];
+        },
+      );
+
+      return { prevScheduled };
+    },
+    onError: (_, __, context) => {
+      if (context?.prevScheduled) {
+        queryClient.setQueryData(
+          scheduledTasksOptions().queryKey,
+          context.prevScheduled,
+        );
+      }
+
+      // Guaranteed there was no in_progres task before this, so this is the prev state.
+      queryClient.setQueryData(inProgressTaskOptions().queryKey, []);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(inProgressTaskOptions());
+
+      queryClient.invalidateQueries(scheduledTasksOptions());
     },
   });
 };
