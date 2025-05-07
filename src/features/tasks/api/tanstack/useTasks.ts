274ad5ef5d158df0e6c357f49189576ee13ef40d
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-05-07 09:03:52 -0600

    [842] Added generics to getTask

--- src/features/tasks/api/tanstack/useTasks.ts
+++ src/features/tasks/api/tanstack/useTasks.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 
+import { TaskAPI } from "../../types/task";
 import { TaskOptions } from "../../types/taskOptions";
 import { getTasks } from "../axios/getTasks";
 import { taskKeys } from "../queryKeys";
@@ -7,6 +8,6 @@ import { taskKeys } from "../queryKeys";
 export const useTasks = ({ filter, sort }: TaskOptions = {}) => {
   return useQuery({
     queryKey: taskKeys.list({ filter, sort }),
-    queryFn: getTasks,
+    queryFn: getTasks<TaskAPI[]>,
   });
 };
