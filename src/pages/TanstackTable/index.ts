27466462dcadd7abbcc748103e9359b73f0d9f12
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-05-24 16:05:48 -0600

    [927] Remove table examples

--- src/pages/TanstackTable/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./TanstackTableExperimentsPage";
-export * from "./experiments/FirstAttempt";
-export * from "./experiments/GlobalSearchTable";
-export * from "./experiments/SortableTable";
