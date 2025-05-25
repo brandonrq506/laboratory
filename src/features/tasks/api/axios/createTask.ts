25ce7107b1177fdfdaa5bd62ff028f20a3d6b404
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-05-24 22:21:13 -0600

    [934] Update create task endpoint

--- src/features/tasks/api/axios/createTask.ts
+++ src/features/tasks/api/axios/createTask.ts
@@ -1,10 +1,8 @@
 import { TASKS_ENDPOINT, apiV1 } from "@/libs/axios";
-import { ActivityAPI } from "@/features/activities/types/activityAPI";
 import { TaskAPI } from "../../types/task";
+import { TaskModel } from "../../types/taskModel";
 
-export const createTask = async (activity: ActivityAPI) => {
-  const { data } = await apiV1.post<TaskAPI>(TASKS_ENDPOINT, {
-    activity_id: activity.id,
-  });
+export const createTask = async (task: Partial<TaskModel>) => {
+  const { data } = await apiV1.post<TaskAPI>(TASKS_ENDPOINT, task);
   return data;
 };
