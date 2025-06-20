502034d184612d239e98633c50e2f76d43ba408c
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-06-19 21:57:05 -0600

    [1027] Remove unused code after DnD

--- src/features/tasks/components/ScheduledTask.tsx
+++ src/features/tasks/components/ScheduledTask.tsx
@@ -1,7 +1,6 @@
 import { Card } from "@/components/layout";
 import { ClockIcon } from "@heroicons/react/24/outline";
 import { Dot } from "@/components/core";
-import { ScheduledTaskActionMenu } from "./ScheduledTaskActionMenu";
 import { ScheduledTaskWithExpectedStartTime } from "../types/scheduledTaskWithExpectedStartTime";
 
 import { formatDatetimeTo12hTime, secondsToTime } from "@/utils";
@@ -15,33 +14,27 @@ export const ScheduledTask = ({ task }: Props) => {
   const color = getColorByName(task.activity.category.color);
 
   return (
-    <Card className="flex justify-between shadow-xs">
-      <div>
-        <div className="flex items-center gap-1.5">
-          <Dot sizeStyles="size-2" colorStyles={color.fillClass} />
-          <p className="text-sm font-semibold">{task.activity.name}</p>
-        </div>
-
-        <div className="flex gap-2.5">
-          {task.expected_start_time && (
-            <div className="flex gap-1 text-xs text-gray-600">
-              <p className="tabular-nums">
-                {formatDatetimeTo12hTime(
-                  task.expected_start_time.toISOString(),
-                )}
-              </p>
-            </div>
-          )}
-          <div className="flex gap-1 text-xs text-gray-600">
-            <ClockIcon className="size-4" />
-            <p className="tabular-nums">
-              {secondsToTime(task.activity.exp_seconds)}
-            </p>
-          </div>
-        </div>
+    <Card className="shadow-xs">
+      <div className="flex items-center gap-1.5">
+        <Dot sizeStyles="size-2" colorStyles={color.fillClass} />
+        <p className="text-sm font-semibold">{task.activity.name}</p>
       </div>
 
-      <ScheduledTaskActionMenu task={task} />
+      <div className="flex gap-2.5">
+        {task.expected_start_time && (
+          <div className="flex gap-1 text-xs text-gray-600">
+            <p className="tabular-nums">
+              {formatDatetimeTo12hTime(task.expected_start_time.toISOString())}
+            </p>
+          </div>
+        )}
+        <div className="flex gap-1 text-xs text-gray-600">
+          <ClockIcon className="size-4" />
+          <p className="tabular-nums">
+            {secondsToTime(task.activity.exp_seconds)}
+          </p>
+        </div>
+      </div>
     </Card>
   );
 };
