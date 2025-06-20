9e26b65ae3f7a579d308f67dfcac9018e32fde21
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-06-19 22:16:35 -0600

    [1034] Card is completely draggable

--- src/features/tasks/components/DragHandle.tsx
+++ src/features/tasks/components/DragHandle.tsx
@@ -1,26 +1,9 @@
 import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/outline";
 import { IconButton } from "@/components/core/Button/IconButton";
 
-import { DraggableAttributes } from "@dnd-kit/core";
-import { SyntheticListenerMap } from "@dnd-kit/core/dist/hooks/utilities";
-
-interface Props {
-  attributes: DraggableAttributes;
-  listeners: SyntheticListenerMap | undefined;
-  setActivatorNodeRef: (element: HTMLElement | null) => void;
-}
-
-export const DragHandle = ({
-  attributes,
-  listeners,
-  setActivatorNodeRef,
-}: Props) => {
+export const DragHandle = () => {
   return (
-    <IconButton
-      {...attributes}
-      {...listeners}
-      ref={setActivatorNodeRef}
-      style={{ touchAction: "none" }}>
+    <IconButton>
       <AdjustmentsHorizontalIcon className="size-5 cursor-grab text-gray-400 hover:text-gray-600" />
       <span className="sr-only">Drag handle</span>
     </IconButton>
