a91c84abb333c33897acb6f9e98aa8f2d8a05c2c
brandonrq506 <91104615+brandonrq506@users.noreply.github.com>
2025-06-19 23:34:34 -0600

    Testing fix on mobile scroll (#1037)

--- src/features/tasks/components/DragHandle.tsx
+++ src/features/tasks/components/DragHandle.tsx
@@ -1,9 +1,26 @@
 import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/outline";
 import { IconButton } from "@/components/core/Button/IconButton";
 
-export const DragHandle = () => {
+import { DraggableAttributes } from "@dnd-kit/core";
+import { SyntheticListenerMap } from "@dnd-kit/core/dist/hooks/utilities";
+
+interface Props {
+  attributes: DraggableAttributes;
+  listeners: SyntheticListenerMap | undefined;
+  setActivatorNodeRef: (element: HTMLElement | null) => void;
+}
+
+export const DragHandle = ({
+  attributes,
+  listeners,
+  setActivatorNodeRef,
+}: Props) => {
   return (
-    <IconButton>
+    <IconButton
+      {...attributes}
+      {...listeners}
+      ref={setActivatorNodeRef}
+      style={{ touchAction: "none" }}>
       <AdjustmentsHorizontalIcon className="size-5 cursor-grab text-gray-400 hover:text-gray-600" />
       <span className="sr-only">Drag handle</span>
     </IconButton>
