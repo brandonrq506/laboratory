39cf4e1d1cef9ec9dfbac9d998cfcbf326d2fbc0
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-07-22 02:53:35 -0600

    [1088] Complete task will max end at the end of day

---
+++ src/features/timer/utils/calculateTimesOnCompletion.ts
@@ -0,0 +1,18 @@
+import { floorSeconds } from "@/utils";
+
+import { endOfDay, min, roundToNearestMinutes } from "date-fns";
+
+export const calculateTimesOnCompletion = (start_time: string) => {
+  const startTime = new Date(start_time);
+
+  /*
+    endOfDay would return '23:59:59', if roundToNearestMinutes, it would go over to the next day at '00:00:00'.
+    So we floor the seconds to ensure it stays within the same day. New Date() does not have that problem, since to be selected, it must be less than '23:59:00'. Also, at completion we want to round both start and end times.
+  */
+  const endTime = min([new Date(), floorSeconds(endOfDay(startTime))]);
+
+  return {
+    start_time: roundToNearestMinutes(startTime).toISOString(),
+    end_time: roundToNearestMinutes(endTime).toISOString(),
+  };
+};
