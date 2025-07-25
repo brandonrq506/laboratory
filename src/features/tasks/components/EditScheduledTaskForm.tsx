e39368adbf96beb35a51e643ab13647780c71545
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-07-25 17:53:32 -0600

    [607] Scheduled task form

---
+++ src/features/tasks/components/EditScheduledTaskForm.tsx
@@ -0,0 +1,32 @@
+import { useNavigate } from "react-router";
+import { useUpdateTask } from "../api/tanstack/useUpdateTask";
+
+import { EditNoteForm } from "../types/editNoteForm";
+import { ScheduledTaskAPI } from "../types/scheduledTask";
+import { ScheduledTaskForm } from "./ScheduledTaskForm";
+
+interface Props {
+  task: ScheduledTaskAPI;
+}
+
+export const EditScheduledTaskForm = ({ task }: Props) => {
+  const navigate = useNavigate();
+  const { mutate } = useUpdateTask();
+
+  const handleSubmit = (data: EditNoteForm) => {
+    mutate({
+      taskId: task.id,
+      task: { note: data.note },
+    });
+
+    navigate(-1);
+  };
+
+  return (
+    <ScheduledTaskForm
+      task={task}
+      onSubmit={handleSubmit}
+      initialValues={{ note: task.note }}
+    />
+  );
+};
