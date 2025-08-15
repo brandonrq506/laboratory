1fa3c51276ae017a35ee8f80064ee03bd710d434
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-08-15 00:27:57 -0600

    [1134] Fixed hook name typo

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,5 @@
 export { useDisclosure } from "./useDisclosure";
-export { useKeywordVisible } from "./useKeyboardVisible";
+export { useKeyboardVisible } from "./useKeyboardVisible";
 export { useOnlineStatus } from "./useOnlineStatus";
 export { useStopwatch } from "./useStopwatch";
 export { useTimer } from "./useTimer";
