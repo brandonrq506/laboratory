2bc0188766dd4a02f1738089242d00947fdb82a6
brandonrq506 <91104615+brandonrq506@users.noreply.github.com>
2025-08-24 14:43:12 -0600

    [1146] Tasks are not optimistically removed from lists.

--- src/features/tasks/api/tanstack/useDeleteTask.ts
+++ src/features/tasks/api/tanstack/useDeleteTask.ts
@@ -1,6 +1,8 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
+import { BaseTaskAPI } from "../../types/baseTask";
 import { deleteTask } from "../axios/deleteTask";
+import { taskDetailsOptions } from "../queryOptions";
 import { taskKeys } from "../queryKeys";
 
 export const useDeleteTask = () => {
@@ -8,8 +10,50 @@ export const useDeleteTask = () => {
 
   return useMutation({
     mutationFn: deleteTask,
-    onSuccess() {
+    onMutate: async (taskId) => {
+      const listsQuery = { queryKey: taskKeys.lists() };
+      const detailQuery = { queryKey: taskKeys.detail(taskId) };
+
+      await queryClient.cancelQueries(listsQuery);
+      await queryClient.cancelQueries(detailQuery);
+
+      const prevQueries = queryClient.getQueriesData(listsQuery);
+      const prevDetail = queryClient.getQueryData(
+        taskDetailsOptions(taskId).queryKey,
+      );
+
+      // Remove task from all lists optimistically
+      queryClient.setQueriesData(listsQuery, (oldTasks: BaseTaskAPI[]) => {
+        return oldTasks.filter((oldTask) => oldTask.id !== taskId);
+      });
+
+      // Remove task detail cache
+      queryClient.removeQueries({ queryKey: taskKeys.detail(taskId) });
+
+      return { prevQueries, prevDetail };
+    },
+
+    onError: (_, taskId, context) => {
+      if (context?.prevQueries) {
+        context.prevQueries.forEach(([queryKey, prevData]) => {
+          queryClient.setQueryData(queryKey, prevData);
+        });
+      }
+
+      if (context?.prevDetail) {
+        queryClient.setQueryData(
+          taskDetailsOptions(taskId).queryKey,
+          context.prevDetail,
+        );
+      }
+    },
+
+    onSettled: (_, __, taskId) => {
+      // Invalidate all task queries to ensure consistency
       queryClient.invalidateQueries({ queryKey: taskKeys.all });
+
+      //Ensure the deleted task's detail query is completely removed since the task no longer exists
+      queryClient.removeQueries({ queryKey: taskKeys.detail(taskId) });
     },
   });
 };
