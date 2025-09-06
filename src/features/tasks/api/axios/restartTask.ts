751da9e15132d32a1798889ffd8f36f65495088a
brandonrq506 <91104615+brandonrq506@users.noreply.github.com>
2025-09-06 13:36:33 -0600

    [1201] Remove unused restartTask functionality (#1221)

--- src/features/tasks/api/axios/restartTask.ts
+++
@@ -1,8 +0,0 @@
-import { TASKS_ENDPOINT, apiV1 } from "@/libs/axios";
-import { ScheduledTaskAPI } from "../../types/scheduledTask";
-
-export const restartTask = async (taskId: number) => {
-  const URL = `${TASKS_ENDPOINT}/${taskId}/restart`;
-  const response = await apiV1.post<ScheduledTaskAPI>(URL);
-  return response.data;
-};
