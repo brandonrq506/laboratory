6016aa870918f08309b1be231c3862b8301a2b1e
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-09-07 16:00:48 -0600

    [1227] Removed outer p tag.

--- src/components/layout/main/MainErrorPage.tsx
+++ src/components/layout/main/MainErrorPage.tsx
@@ -37,9 +37,7 @@ export const MainErrorPage = () => {
           <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
             {error.message}
           </h1>
-          <p>
-            <pre>{error.stack}</pre>
-          </p>
+          <pre>{error.stack}</pre>
           <p>{error.name}</p>
           <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
             This issue will be reported to our support team. Please try again
