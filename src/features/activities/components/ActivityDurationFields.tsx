4eba1835d6629fa57228f1d31ac0c04a44bca3bf
brandonrq506 <91104615+brandonrq506@users.noreply.github.com>
2025-09-15 16:37:55 -0600

    [1282] Refactored ActivityForm component (#1283)

---
+++ src/features/activities/components/ActivityDurationFields.tsx
@@ -0,0 +1,125 @@
+import { useFormContext } from "react-hook-form";
+
+import { CreateForm } from "../types/createForm";
+import { NumberInput } from "@/components/form";
+
+export const ActivityDurationFields = () => {
+  const {
+    formState: { errors },
+    getValues,
+    register,
+  } = useFormContext<CreateForm>();
+
+  return (
+    <>
+      <div>
+        <div className="flex gap-1">
+          <NumberInput
+            label="Exp. Hours"
+            placeholder="00"
+            hideErrorMessage
+            className="flex-auto"
+            error={errors.exp_time_hours?.message}
+            registration={register("exp_time_hours", {
+              required: "At least 0 is required",
+              valueAsNumber: true,
+              min: {
+                value: 0,
+                message: "Hours can't be negative",
+              },
+              max: {
+                value: 23,
+                message: "Can't be longer than a day",
+              },
+            })}
+          />
+          <NumberInput
+            label="Exp. Minutes"
+            placeholder="30"
+            hideErrorMessage
+            className="flex-auto"
+            error={errors.exp_time_minutes?.message}
+            registration={register("exp_time_minutes", {
+              required: "At least 0 is required",
+              valueAsNumber: true,
+              min: {
+                value: 0,
+                message: "Minutes can't be negative",
+              },
+              max: {
+                value: 59,
+                message: "Minutes must be less than an hour",
+              },
+              validate: {
+                isNotZero: (value) => {
+                  const { exp_time_hours } = getValues();
+                  if (exp_time_hours === 0 && value === 0) {
+                    return "Activities should take at least 1 minute";
+                  }
+                },
+              },
+            })}
+          />
+        </div>
+        <div role="alert" className="mt-2 text-sm font-light text-red-600">
+          {errors.exp_time_hours?.message || errors.exp_time_minutes?.message}
+        </div>
+      </div>
+
+      <div>
+        <div className="flex gap-1">
+          <NumberInput
+            label="Max. Hours"
+            placeholder="01"
+            hideErrorMessage
+            className="flex-auto"
+            error={errors.max_time_hours?.message}
+            registration={register("max_time_hours", {
+              required: "At least 0 is required",
+              valueAsNumber: true,
+              min: {
+                value: 0,
+                message: "Hours can't be negative",
+              },
+              max: {
+                value: 23,
+                message: "Can't be longer than a day",
+              },
+            })}
+          />
+
+          <NumberInput
+            label="Max. Minutes"
+            placeholder="30"
+            hideErrorMessage
+            className="flex-auto"
+            error={errors.max_time_minutes?.message}
+            registration={register("max_time_minutes", {
+              required: "At least 0 is required",
+              valueAsNumber: true,
+              min: {
+                value: 0,
+                message: "Minutes can't be negative",
+              },
+              max: {
+                value: 59,
+                message: "Minutes must be less than an hour",
+              },
+              validate: {
+                isNotZero: (value) => {
+                  const { max_time_hours } = getValues();
+                  if (max_time_hours === 0 && value === 0) {
+                    return "Activities should take at least 1 minute";
+                  }
+                },
+              },
+            })}
+          />
+        </div>
+        <div role="alert" className="mt-2 text-sm font-light text-red-600">
+          {errors.max_time_hours?.message || errors.max_time_minutes?.message}
+        </div>
+      </div>
+    </>
+  );
+};
--- src/features/activities/components/ActivityForm.tsx
+++ src/features/activities/components/ActivityForm.tsx
@@ -1,6 +1,7 @@
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 
-import { NumberInput, TextInput } from "@/components/form";
+import { ActivityDurationFields } from "./ActivityDurationFields";
+import { ActivityNameFields } from "./ActivityNameFields";
 import { Button } from "@/components/core";
 import { CategorySelect } from "@/features/categories/components";
 import { CreateForm } from "../types/createForm";
@@ -26,154 +27,35 @@ export const ActivityForm = ({
   onSubmit,
   submitButtonText,
 }: Props) => {
-  const { control, formState, getValues, handleSubmit, register } =
-    useForm<CreateForm>({
-      values: { ...defaultActivity, ...initialValues },
-    });
-  const { errors, isSubmitting } = formState;
+  const methods = useForm<CreateForm>({
+    values: { ...defaultActivity, ...initialValues },
+  });
+  const {
+    control,
+    formState: { isSubmitting },
+  } = methods;
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="grid gap-2 sm:grid-cols-2 sm:gap-4">
-        <TextInput
-          autoFocus
-          showAsterisk
-          label="Name"
-          placeholder="Shower"
-          description="Used for reports and exports."
-          error={errors.name?.message}
-          registration={register("name", { required: "Name is required" })}
-        />
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>
+        <div className="grid gap-2 sm:grid-cols-2 sm:gap-4">
+          <ActivityNameFields />
 
-        <TextInput
-          label="Display Name"
-          placeholder="Shower"
-          description="What you will see on the UI."
-          error={errors.display_name?.message}
-          registration={register("display_name")}
-        />
+          <CategorySelect
+            control={control}
+            name="category_id"
+            rules={{ required: "A category must be selected" }}
+          />
 
-        <CategorySelect
-          control={control}
-          name="category_id"
-          rules={{ required: "A category must be selected" }}
-        />
-
-        <div>
-          <div className="flex gap-1">
-            <NumberInput
-              label="Exp. Hours"
-              placeholder="00"
-              hideErrorMessage
-              className="flex-auto"
-              error={errors.exp_time_hours?.message}
-              registration={register("exp_time_hours", {
-                required: "At least 0 is required",
-                valueAsNumber: true,
-                min: {
-                  value: 0,
-                  message: "Hours can't be negative",
-                },
-                max: {
-                  value: 23,
-                  message: "Can't be longer than a day",
-                },
-              })}
-            />
-            <NumberInput
-              label="Exp. Minutes"
-              placeholder="30"
-              hideErrorMessage
-              className="flex-auto"
-              error={errors.exp_time_minutes?.message}
-              registration={register("exp_time_minutes", {
-                required: "At least 0 is required",
-                valueAsNumber: true,
-                min: {
-                  value: 0,
-                  message: "Minutes can't be negative",
-                },
-                max: {
-                  value: 59,
-                  message: "Minutes must be less than an hour",
-                },
-                validate: {
-                  isNotZero: (value) => {
-                    const { exp_time_hours } = getValues();
-                    if (exp_time_hours === 0 && value === 0) {
-                      return "Activities should take at least 1 minute";
-                    }
-                  },
-                },
-              })}
-            />
-          </div>
-          <div role="alert" className="mt-2 text-sm font-light text-red-600">
-            {errors.exp_time_hours?.message || errors.exp_time_minutes?.message}
-          </div>
+          <ActivityDurationFields />
         </div>
 
-        <div>
-          <div className="flex gap-1">
-            <NumberInput
-              label="Max. Hours"
-              placeholder="01"
-              hideErrorMessage
-              className="flex-auto"
-              error={errors.max_time_hours?.message}
-              registration={register("max_time_hours", {
-                required: "At least 0 is required",
-                valueAsNumber: true,
-                min: {
-                  value: 0,
-                  message: "Hours can't be negative",
-                },
-                max: {
-                  value: 23,
-                  message: "Can't be longer than a day",
-                },
-              })}
-            />
-
-            <NumberInput
-              label="Max. Minutes"
-              placeholder="30"
-              hideErrorMessage
-              className="flex-auto"
-              error={errors.max_time_minutes?.message}
-              registration={register("max_time_minutes", {
-                required: "At least 0 is required",
-                valueAsNumber: true,
-                min: {
-                  value: 0,
-                  message: "Minutes can't be negative",
-                },
-                max: {
-                  value: 59,
-                  message: "Minutes must be less than an hour",
-                },
-                validate: {
-                  isNotZero: (value) => {
-                    const { max_time_hours } = getValues();
-                    if (max_time_hours === 0 && value === 0) {
-                      return "Activities should take at least 1 minute";
-                    }
-                  },
-                },
-              })}
-            />
-          </div>
-          <div role="alert" className="mt-2 text-sm font-light text-red-600">
-            {errors.max_time_hours?.message || errors.max_time_minutes?.message}
-          </div>
+        <div className="mt-2 flex justify-center">
+          <Button type="submit" isLoading={isSubmitting}>
+            {submitButtonText}
+          </Button>
         </div>
-      </div>
-
-      <div className="mt-2 flex justify-center">
-        <Button type="submit" isLoading={isSubmitting}>
-          {submitButtonText}
-        </Button>
-      </div>
-    </form>
+      </form>
+    </FormProvider>
   );
 };
