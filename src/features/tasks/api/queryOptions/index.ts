7d7feb7fe1749ec962875e987a0fe75e5e2ff3f8
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-09-21 00:39:17 -0600

    [1296] Added new query Options

--- src/features/tasks/api/queryOptions/index.ts
+++ src/features/tasks/api/queryOptions/index.ts
@@ -1,3 +1,4 @@
+export { historyTasksOptions } from "./historyTaskOptions";
 export { inProgressTaskOptions } from "./inProgressTaskOptions";
 export { scheduledTasksOptions } from "./scheduledTasksOptions";
 export { taskDetailsOptions } from "./taskDetailsOptions";
