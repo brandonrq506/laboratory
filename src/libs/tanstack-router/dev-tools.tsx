cbe4019cd29418a02c70594db0415a6b9dbb7e83
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-10-04 09:04:51 -0600

    [1361] Installing devtools

---
+++ src/libs/tanstack-router/dev-tools.tsx
@@ -0,0 +1,6 @@
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+
+export const TanStackRouterDevtools = {
+  name: "Tanstack Router",
+  render: <TanStackRouterDevtoolsPanel />,
+};
