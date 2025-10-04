8fdfdd86328091c1f492024b3b7f3f03ff570106
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-10-04 16:24:54 -0600

    [1361] Main Configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,56 @@
 import "./index.css";
-import { App } from "@/app/";
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+import { AuthProvider, useAuth } from "./features/auth/stores";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./libs/tanstack-query/query-client";
+
+// Create a new router instance
+const TanStackQueryProviderContext = { queryClient };
+
+const router = createRouter({
+  routeTree,
+  defaultPreload: "intent",
+  scrollRestoration: true,
+  defaultStructuralSharing: true,
+  defaultPreloadStaleTime: 0,
+  context: {
+    ...TanStackQueryProviderContext,
+    auth: undefined!,
+  },
+});
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+function InnerApp() {
+  const auth = useAuth();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} context={{ auth }} />
+    </QueryClientProvider>
+  );
+}
+
+export function App() {
+  return (
+    <AuthProvider>
+      <InnerApp />
+    </AuthProvider>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    tanstackRouter({ autoCodeSplitting: true }),
+    tanstackRouter({
+      autoCodeSplitting: true,
+      quoteStyle: "double",
+      semicolons: true,
+    }),
     react(),
     tsconfigPaths(),
     tailwindcss(),
