6d95ac182fb9afe07348005f8ab258891089b86e
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-10-12 16:00:29 -0600

    [1361] Replace route after creating a new routine

--- src/features/routines/components/CreateRoutineForm.tsx
+++ src/features/routines/components/CreateRoutineForm.tsx
@@ -19,6 +19,7 @@ export const CreateRoutineForm = () => {
           navigate({
             to: "/routines/$routineId/edit",
             params: { routineId: String(data.id) },
+            replace: true,
           });
         },
       },
