b1a45bca6e4ca6a696cbb8be51c7c31234c3eab7
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-10-14 08:55:26 -0600

    [1383] Add env guard to DevTools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,7 @@
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
 
 import { Fragment } from "react/jsx-runtime";
-import { TanStackDevtools } from "@tanstack/react-devtools";
-import { TanStackQueryDevtools } from "@/libs/tanstack-query/dev-tools";
-import { TanStackRouterDevtools } from "@/libs/tanstack-router/dev-tools";
+import { TanStackDevtoolsWrapper } from "@/libs/tanstack-devtools";
 
 import type { AuthContextType } from "@/features/auth/stores/AuthContextType";
 import { NotFoundPage } from "@/pages/NotFoundPage";
@@ -18,9 +16,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <Fragment>
       <Outlet />
-      <TanStackDevtools
-        plugins={[TanStackRouterDevtools, TanStackQueryDevtools]}
-      />
+      <TanStackDevtoolsWrapper />
     </Fragment>
   ),
   notFoundComponent: () => <NotFoundPage />,
