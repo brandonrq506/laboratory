6571a085a35818b32a4c66be373cb669c0ca0c42
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-10-18 12:04:33 -0600

    [1334] Fix prettier

--- src/routes/__protected/routines/$routineId/edit.tsx
+++ src/routes/__protected/routines/$routineId/edit.tsx
@@ -27,7 +27,7 @@ function RouteComponent() {
       isOpen={isOpen}
       onClose={navigateBack}
       className="flex max-h-[calc(100dvh-2rem)] min-h-0 flex-col overflow-visible sm:max-h-[calc(100dvh-4rem)]">
-      <div className="flex flex-1 min-h-0 flex-col gap-6">
+      <div className="flex min-h-0 flex-1 flex-col gap-6">
         <div className="flex flex-col gap-4">
           <HeadingMedium
             title="Edit Routine"
@@ -39,7 +39,7 @@ function RouteComponent() {
             action={<AddActivityRoutineMenu routineId={routineId} />}
           />
         </div>
-        <div className="flex flex-1 min-h-0 flex-col gap-6">
+        <div className="flex min-h-0 flex-1 flex-col gap-6">
           <div className="flex-1 overflow-y-auto px-3 [scrollbar-gutter:stable]">
             <RoutineActivityList />
           </div>
