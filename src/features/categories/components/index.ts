0aa337228bb0d9f8bcb8fa8b60a8b145eaf76372
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-10-18 12:58:53 -0600

    [1375] Removed unused files

--- src/features/categories/components/index.ts
+++ src/features/categories/components/index.ts
@@ -1,7 +1,6 @@
 export { CategoryBadge } from "./CategoryBadge";
 export { CategorySelect } from "./CategorySelect";
 export { CreateCategoryForm } from "./CreateCategoryForm";
-export { DeleteCategory } from "./DeleteCategory";
 export { DeleteCategoryDialog } from "./DeleteCategoryDialog";
 export { EditCategoryForm } from "./EditCategoryForm";
 export { NewCategoryButton } from "./NewCategoryButton";
