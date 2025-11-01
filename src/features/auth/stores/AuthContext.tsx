b59c4c0874ddb6f98e5e0c15d89ec0c6d297b57a
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-11-01 10:20:58 -0600

    [1450] Removed the core of the old auth feature

--- src/features/auth/stores/AuthContext.tsx
+++
@@ -1,6 +0,0 @@
-import { AuthContextType } from "./AuthContextType";
-import { createContext } from "react";
-
-export const AuthContext = createContext<AuthContextType | undefined>(
-  undefined,
-);
