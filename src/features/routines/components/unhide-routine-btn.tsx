8761c04306801499bdd2c3a00d768ba441ae3820
Brandon Ramirez <darkrahd@gmail.com>
2026-02-14 16:48:58 -0600

    [1591] Correct visibility icons

--- src/features/routines/components/unhide-routine-btn.tsx
+++ src/features/routines/components/unhide-routine-btn.tsx
@@ -1,6 +1,6 @@
 import { useUnhideRoutine } from "../api/tanstack/use-unhide-routine";
 
-import { EyeIcon } from "@heroicons/react/24/outline";
+import { EyeSlashIcon } from "@heroicons/react/24/outline";
 import { IconButton } from "@/components/core";
 
 import { ROUTINE } from "@/constants/entities";
@@ -21,7 +21,7 @@ export const UnhideRoutineButton = ({ routineId }: Props) => {
       aria-label={BTN_TEXT}
       variant="primaryOutline"
       onClick={() => mutate(routineId)}>
-      <EyeIcon className="size-5" aria-hidden />
+      <EyeSlashIcon className="size-5" aria-hidden />
     </IconButton>
   );
 };
