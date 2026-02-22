a55fe8fdef7aea49e77208b4f30d3e5f5b020cd8
Brandon Ramirez <darkrahd@gmail.com>
2026-02-22 00:01:20 -0600

    [1472] Remove legacy modal

--- src/features/categories/components/index.ts
+++ src/features/categories/components/index.ts
@@ -1,6 +1,5 @@
 export { CategoryBadge } from "./CategoryBadge";
 export { CategorySelect } from "./CategorySelect";
 export { CreateCategoryForm } from "./CreateCategoryForm";
-export { DeleteCategoryDialog } from "./DeleteCategoryDialog";
 export { EditCategoryForm } from "./EditCategoryForm";
 export { NewCategoryButton } from "./NewCategoryButton";
