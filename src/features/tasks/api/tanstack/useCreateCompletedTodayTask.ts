bc0c565b658046e3fbe2138d43ecdf980e44e8bf
Brandon Ramirez <darkrahd@gmail.com>
2026-08-22 19:13:46 -0600

    [1838] Remove optimistic update from create completed task today

--- src/features/tasks/api/tanstack/useCreateCompletedTodayTask.ts
+++ src/features/tasks/api/tanstack/useCreateCompletedTodayTask.ts
@@ -1,66 +1,50 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import {
-  getActivityFromCache,
-  snapshotQueries,
-} from "@/utils/tanstack/helpers";
-import { buildTemporaryCompletedTask } from "../../utils/buildTemporaryCompletedTask";
+  taskByIdQueryOptions,
+  taskKeys,
+  todayCompletedTasksQueryOptions,
+} from "../queries";
 import { createTask } from "../axios/createTask";
 import { isBefore } from "date-fns";
-import { todayCompletedTasksQueryOptions } from "../queries";
+
+import { TASK_STATUS } from "@/features/tasks/types/task-status";
 
 export const useCreateCompletedTodayTask = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: createTask,
-    onMutate: async (newTask) => {
-      const completedTaskKeys = todayCompletedTasksQueryOptions().queryKey;
+    onSuccess: (createdTask) => {
+      // Just in case someone calls this hook and provides a task that is not completed.
+      if (createdTask.status !== TASK_STATUS.COMPLETED) {
+        queryClient.invalidateQueries({ queryKey: taskKeys.all });
+        return;
+      }
 
-      await queryClient.cancelQueries(todayCompletedTasksQueryOptions());
+      const completedKey = todayCompletedTasksQueryOptions().queryKey;
 
-      const { rollback } = snapshotQueries(queryClient, [completedTaskKeys]);
+      queryClient.setQueryData(completedKey, (previousTasks) => {
+        if (!previousTasks) return [createdTask];
 
-      const activity = getActivityFromCache(queryClient, newTask.activity_id);
-      if (!activity) return { rollback };
-
-      const newCompletedTask = buildTemporaryCompletedTask(
-        activity,
-        newTask.start_time ?? new Date().toISOString(),
-        newTask.end_time ?? new Date().toISOString(),
-        newTask.note || "",
-      );
-
-      /*
-          Add new completed task to today's completed tasks cache
-          Since it is sorted by start_time in descending order.
-          isBefore is true when task.start_time is older than the new one.
-          So you insert before this older task to keep the list in newest-to-oldest order.
-       */
-
-      queryClient.setQueryData(completedTaskKeys, (previousTasks) => {
-        if (!previousTasks) return [newCompletedTask];
-
+        // Is this existing task older than the newly created task? Stops at the first task where the answer is true and returns its position.
         const index = previousTasks.findIndex((task) =>
-          isBefore(task.start_time, newCompletedTask.start_time!),
+          isBefore(task.start_time, createdTask.start_time),
         );
 
-        if (index === -1) return [newCompletedTask, ...previousTasks];
+        if (index === -1) return [...previousTasks, createdTask];
 
         return [
           ...previousTasks.slice(0, index),
-          newCompletedTask,
+          createdTask,
           ...previousTasks.slice(index),
         ];
       });
 
-      return { rollback };
-    },
-    onError: (_, __, context) => {
-      context?.rollback();
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries(todayCompletedTasksQueryOptions());
+      queryClient.setQueryData(
+        taskByIdQueryOptions(createdTask.id).queryKey,
+        createdTask,
+      );
     },
   });
 };
