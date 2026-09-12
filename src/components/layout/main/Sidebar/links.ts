c6a31c4075716d68df1fea46a5bdc3c37fefe9d4
brandonrq506 <91104615+brandonrq506@users.noreply.github.com>
2026-09-11 23:03:55 -0600

    [ad-hoc] Timer and History to be first links on the sidebar (#1860)

--- src/components/layout/main/Sidebar/links.ts
+++ src/components/layout/main/Sidebar/links.ts
@@ -9,7 +9,6 @@ import { getToday } from "@/utils";
 import { linkOptions } from "@tanstack/react-router";
 
 export const sidebarLinks = linkOptions([
-  { label: "Activities", to: "/activities", icon: UsersIcon },
   { label: "Timer", to: "/timer", icon: ClockIcon },
   {
     label: "History",
@@ -17,6 +16,7 @@ export const sidebarLinks = linkOptions([
     icon: CalendarIcon,
     search: () => ({ date: getToday() }),
   },
+  { label: "Activities", to: "/activities", icon: UsersIcon },
   { label: "Routines", to: "/routines", icon: QueueListIcon },
   { label: "Settings", to: "/settings", icon: Cog6ToothIcon },
 ]);
