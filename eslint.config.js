03387f1b45f367a7463a9862ab485be68309cc83
Brandon Ramirez <darkrahd@gmail.com>
2026-09-12 11:04:57 -0600

    [1867] Remove dead dependency

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
 import pluginQuery from "@tanstack/eslint-plugin-query";
 import testingLibrary from "eslint-plugin-testing-library";
 import eslintConfigPrettier from "eslint-config-prettier";
@@ -37,7 +36,6 @@ export default [
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat["jsx-runtime"],
   eslintConfigPrettier,
   {
     rules: {
